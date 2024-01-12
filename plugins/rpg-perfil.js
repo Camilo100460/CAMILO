d0ce72a0050b2110bea4595b7ee591173991b516
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-12 23:51:21 +0100

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -13,7 +13,7 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
     const username = conn.getName(who);
     const prem = global.prems.includes(who.split `@` [0]);
     const sn = createHash('md5').update(who).digest('hex');
-    const str = `         |•_*PERFIL INFO*_•|📖
+    const str = `         |•_*PERFIL INFO*_•| 📖
     
 📝• _*NOMBRE:*_ ${username} ${registered ? '(' + name + ') ': ''}
 📞• _*NUMERO:*_ ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
@@ -23,7 +23,7 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
 🌟• _*PREMIUM:*_ ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
 🧮• _*NUMERO DE SERIE:*_ 
 ${sn}
-★ ━━━━━━━━━━━━━━━ ★`;
+★ ━━━━━━━━━━━━━━━━━━ ★`;
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
   }
 };
