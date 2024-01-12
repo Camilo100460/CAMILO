09737def088bc9c18ebce8bed257defb135a2e8f
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-12 01:13:58 +0100

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     } else if (command === 'play2') {
       additionalText = 'vídeo';
     }
-    const texto1 = `_*< DESCARGAS - PLAY />*_\n\n📜• *Título:* ${yt_play[0].title}\n\n📢• *Publicado:* ${yt_play[0].ago}\n\n⏳• *Duración:* ${secondString(yt_play[0].duration.seconds)}\n\n👀• *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}\n\n🖋️• *Autor:* ${yt_play[0].author.name}\n\n🔑• *ID:* ${yt_play[0].videoId}\n\n🔘𝑻𝒊𝒑𝒐• *Tipo:* ${yt_play[0].type}\n\n🔗• *Enlace:* ${yt_play[0].url}\n\n📺• *Canal:* ${yt_play[0].author.url}\n\n*[ ℹ️ ] Se está enviando el ${additionalText}. espere...*`.trim();
+    const texto1 = `_*< DESCARGAS - PLAY />*_\n\n📜• *Título:* ${yt_play[0].title}\n\n📢• *Publicado:* ${yt_play[0].ago}\n\n⏳• *Duración:* ${secondString(yt_play[0].duration.seconds)}\n\n👀• *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}\n\n🖋️• *Autor:* ${yt_play[0].author.name}\n\n🔑• *ID:* ${yt_play[0].videoId}\n\n🔘• *Tipo:* ${yt_play[0].type}\n\n🔗• *Enlace:* ${yt_play[0].url}\n\n📺• *Canal:* ${yt_play[0].author.url}\n\n*[ ℹ️ ] Se está enviando el ${additionalText}. espere...*`.trim();
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {   
