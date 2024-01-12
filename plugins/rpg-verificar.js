d29019c50b903f8dfef0629630fccf63086c0712
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-12 14:45:44 +0100

    Update rpg-verificar.js
    El cambio del registro

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -4,12 +4,12 @@ const handler = async function(m, {conn, text, usedPrefix, command}) {
   const user = global.db.data.users[m.sender];
   const name2 = conn.getName(m.sender);
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.imagen1);
-  if (user.registered === true) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝙴𝚂𝚃𝙰𝚂 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾\n\n¿𝚀𝚄𝙸𝚁𝚁𝙴 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝚁𝚂𝙴?\n\n 📌𝚄𝚂𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙿𝙰𝚁𝙰 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝚁 𝚂𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾\n*${usedPrefix}unreg* <Número de serie>`;
-  if (!Reg.test(text)) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾*\n\n*—◉ 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} nombre.edad*\n*—◉ Ejemplo: ${usedPrefix + command} Shadow.18*`;
+  if (user.registered === true) throw `[❗𝐈𝐍𝐅𝐎❗] _*YA ESTAS REGISTRADO*_\n\n_*¿QUIERE VOLVER A REGISTRARSE?_*\n\n 📌 USE ESTE COMANDO PARA ELIMINAR SU REGISTRO\n*${usedPrefix}unreg* <Número de serie>`;
+  if (!Reg.test(text)) throw `*[❗𝐈𝐍𝐅𝐎❗] FORMATO INCORRECTO*\n\n*🖥️• USO DEL COMANDO: ${usedPrefix + command} nombre.edad*\n*—◉ Ejemplo: ${usedPrefix + command} May.18*`;
   let [_, name, splitter, age] = text.match(Reg);
-  if (!name) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙳𝙴𝙱𝙴𝚂 𝙿𝙾𝙽𝙴𝚁 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴*';
-  if (!age) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙰 𝙴𝙳𝙰𝙳 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚂𝚃𝙰𝚁 𝚅𝙰𝙲𝙸𝙰*';
-  if (name.length >= 30) throw '[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙴𝚂 𝙳𝙴𝙼𝙰𝚂𝙸𝙰𝙳𝙾 𝙻𝙰𝚁𝙶𝙾';
+  if (!name) throw '[❗𝐈𝐍𝐅𝐎❗]_* DEBES PONER UN NOMBRE*_';
+  if (!age) throw '[❗𝐈𝐍𝐅𝐎❗]_* LA EDAD NO PUEDE ESTAR VACIA*_';
+  if (name.length >= 30) throw '[❗𝐈𝐍𝐅𝐎❗]_* EL NOMBRE ES DEMASIADO LARGO*_';
   age = parseInt(age);
   if (age > 100) throw '*[❗] Kheee, como sigues vivo con esa edad? 👴🏻*';
   if (age < 5) throw '*[❗] Kheee, un bebé que sabe usar WhatsApp? 😲*';
@@ -18,18 +18,20 @@ const handler = async function(m, {conn, text, usedPrefix, command}) {
   user.regTime = + new Date;
   user.registered = true;
   const sn = createHash('md5').update(m.sender).digest('hex');
-  const caption = `┏┅ ━━━━━━━━━━━━ ┅ ━
-┇「 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍 」
-┣┅ ━━━━━━━━━━━━ ┅ ━
-┃ *𝙽𝙾𝙼𝙱𝚁𝙴:* ${name}
-┃ *𝙴𝙳𝙰𝙳:* ${age} años
-┃ *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
-┃ ${sn}
-┣┅ ━━━━━━━━━━━━ ┅ ━
+const caption = `
+┏━━━━━━━━━━━━━━━━━━━━━━━━━
+┇           𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍
+┣━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ 📛 • NOMBRE: ${name}
+┃ 🎂 • EDAD: ${age} años
+┃ 🔢 • NUMERO DE SERIE: 
+┃     ${sn}
+┣━━━━━━━━━━━━━━━━━━━━━━━━━
 ┃ ¡𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝚃𝙴 𝚂𝙴𝚁𝚅𝙸𝚁𝙰 
 ┃ 𝙿𝙾𝚁 𝚂𝙸 𝙳𝙴𝚂𝙴𝙰𝚂 𝙱𝙾𝚁𝚁𝙰𝚁 
 ┃ 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃!
-┗┅ ━━━━━━━━━━━━ ┅ ━`;
+┗━━━━━━━━━━━━━━━━━━━━━━━━━
+`;
   // let author = global.author
   await conn.sendFile(m.chat, pp, 'mystic.jpg', caption);
   // conn.sendButton(m.chat, caption, `¡𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝚃𝙴 𝚂𝙴𝚁𝚅𝙸𝚁𝙰 𝙿𝙾𝚁 𝚂𝙸 𝙳𝙴𝚂𝙴𝙰𝚂 𝙱𝙾𝚁𝚁𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃!\n${author}`, [['¡¡𝙰𝙷𝙾𝚁𝙰 𝚂𝙾𝚈 𝚄𝙽 𝚅𝙴𝚁𝙸𝙵𝙸𝙲𝙰𝙳𝙾/𝙰!!', '/profile']], m)
