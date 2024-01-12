36c9e5cbc9620b0df0342e59507b34319cede20a
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-12 01:23:32 +0100

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -15,7 +15,7 @@ const handler = async (m, {conn, usedPrefix}) => {
         'mediaUrl': 'https://github.com/Akanksh',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': '*SOLO MAY - BOT⁩',
+        'title': '*SOLO MAY - BOT*⁩',
         'body': wm,
         'thumbnail': imagen1,
         'sourceUrl': 'https://www.youtube.com/watch?v=-yc54n9Tv5A'}},
