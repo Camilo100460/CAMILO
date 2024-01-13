f0fe6fc556d8f1b954215a66248911b6eb26ecf4
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-13 19:58:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -512,7 +512,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = ``;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
