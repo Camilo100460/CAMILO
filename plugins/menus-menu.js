873f82c9b1f367fc0fb7305a40c359acd6846bb9
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-19 06:03:10 +0100

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -26,13 +26,13 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
  📊 _*< TUS ESTADÍSTICAS />*_
 
- 🎖️•  *Nivel:* ${level}
- 📈•  *Experiencia:* ${exp}
- 🏅•  *Rango:* ${role}
- 💎•  *Diamantes:* ${limit}
- 💰•  *MysticCoins:* ${money}
- 🎟️•  *Tokens:* ${joincount}
- 🎁•  *Premium:* ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
+ 🎖️ •  *Nivel:* ${level}
+ 📈 •  *Experiencia:* ${exp}
+ 🏅 •  *Rango:* ${role}
+ 💎 •  *Diamantes:* ${limit}
+ 💰 •  *MysticCoins:* ${money}
+ 🎟️ •  *Tokens:* ${joincount}
+ 🎁 •  *Premium:* ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
  ${readMore}
 
   _*< 📜 LISTA DE MENUS 📋 />*_
