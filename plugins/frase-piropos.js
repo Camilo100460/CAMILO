c06408ba62e33630b95bea7236b2a6e6f5afd264
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-19 02:23:46 +0100

    Update frase-piropos.js

--- plugins/frase-piropos.js
+++ plugins/frase-piropos.js
@@ -1,7 +1,7 @@
 /* By https://github.com/DIEGO-OFC/DORRAT-BOT-MD */
 
 const handler = async (m, {conn, text}) => {
-  m.reply(`*╔═.✵.════════════════╗*\n➢ *"${pickRandom(global.piropo)}"*\n*╚════════════════.✵.═╝*`);
+  m.reply(`*╔═.✵.════════════════╗*\n💐•  *"${pickRandom(global.piropo)}"*\n*╚════════════════.✵.═╝*`);
 };
 handler.tags = ['frases'];
 handler.command = ['piropo'];
