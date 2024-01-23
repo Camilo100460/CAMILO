2041f9b6a9b74efd6a3c66a4b35395f4fb4cc45e
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-01-23 01:06:05 +0100

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -19,6 +19,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
+    
  📌 *Hola,* ${taguser}
 
 ╭━━━━━━ ◜◌◝ ━━━━━━╮
