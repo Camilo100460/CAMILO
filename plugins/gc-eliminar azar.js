91c8d3c26a4a78c7435f3fadee8af4258f99f58e
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-01 18:58:31 +0100

    Update gc-eliminar azar.js

--- plugins/gc-eliminar azar.js
+++ plugins/gc-eliminar azar.js
@@ -1,14 +1,7 @@
 const handler = async (m, {conn, participants}) => {
-  // Obtén la lista de participantes
   const groupMembers = participants.map(participant => participant.jid);
-  
-  // Selecciona un miembro al azar
-  const randomMember = groupMembers[Math.floor(Math.random() * groupMembers.length)];
-  
-  // Asegúrate de no eliminar al bot o al usuario que invoca el comando
+  const randomMember = groupMembers[Math.floor(Math.random() * groupMembers.length)]
   if ([conn.user.jid, m.sender].includes(randomMember)) return;
-  
-  // Elimina al miembro seleccionado
   await conn.groupParticipantsUpdate(m.chat, [randomMember], 'remove');
 };
 
