0953dc697676e6df95964311ce9e11103be87cec
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-02 22:38:35 +0100

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,28 +1,30 @@
-import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
-const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`;
-  try {
-    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
-    const resSimi = await api.json();
-    m.reply(resSimi.success);
-  } catch {
-    try {
-      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-      if (text.includes('hola')) text = text.replace('hola', 'Hello');
-      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
-      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
-      const resu = await reis.json();
-      const nama = m.pushName || '1';
-      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
-      const res = await api.json();
-      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
-      const resu2 = await reis2.json();
-      m.reply(resu2[0][0][0]);
-    } catch {
-      throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙴 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
-    }
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  const name = conn.getName(m.sender);
+  if (!text) {
+    throw `Hi *${name}*, do you want to talk? Respond with *${usedPrefix + command}* (your message)\n\n📌 Example: *${usedPrefix + command}* Hi bot`;
   }
+  
+  m.react('🗣️');
+
+  const msg = encodeURIComponent(text);
+  
+  const res = await fetch(`https://ultimetron.guruapi.tech/rekha?prompt=${msg}`);
+  
+  const json = await res.json();
+  
+  
+  let reply = json.result.response;
+    m.reply(reply);
+
+};
+
+handler.help = ['bot'];
+handler.tags = ['fun'];
+handler.command = ['bot', 'alexa'];
+
+export default handler;
 };
 handler.help = ['simi', 'bot'].map((v) => v + ' <teks>');
 handler.tags = ['fun'];
