8ac4548e9ae61b5fc1d18760a61a75e0047bbf42
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-02 23:24:21 +0100

    Update game-suerte.js

--- plugins/game-suerte.js
+++ plugins/game-suerte.js
@@ -3,9 +3,16 @@ let toM = a => '@' + a.split('@')[0];
 async function handler(m, { groupMetadata }) {
     let ps = groupMetadata.participants.map(v => v.id);
     let b = ps[Math.floor(Math.random() * ps.length)]; // Selecciona aleatoriamente a un participante
+
+    // Envía el mensaje primero
     m.reply(`${toM(b)} serás eliminado`, null, {
         mentions: [b]
     });
+
+    // Espera 1 segundo (puedes ajustar este tiempo)
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Elimina al participante después de enviar el mensaje
     await conn.groupParticipantsUpdate(m.chat, [b], 'remove');
 }
 
