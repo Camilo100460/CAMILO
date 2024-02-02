838c5ad30245b0228e50992bf36e20038fcd6099
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-02 22:40:01 +0100

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,26 +1,20 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
+let handler = async (m, { text, usedPrefix, command }) => {
+let name = await conn.getName(m.sender)
+if (!text) {
+ conn.reply(m.chat, `Hola *@${m.sender.split`@`[0]}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n*_📌 Ejemplo :_* *${usedPrefix + command}* Hola bot`, m, { mentions: [m.sender] })
+ throw false
+ }
+let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+let json = await res.json()
+if (json.success) m.reply(json.success)
+else throw json }
+handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
+handler.tags = ['fun']
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i
+handler.register = true
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  const name = conn.getName(m.sender);
-  if (!text) {
-    throw `Hi *${name}*, do you want to talk? Respond with *${usedPrefix + command}* (your message)\n\n📌 Example: *${usedPrefix + command}* Hi bot`;
-  }
-  
-  m.react('🗣️');
-
-  const msg = encodeURIComponent(text);
-  
-  const res = await fetch(`https://ultimetron.guruapi.tech/rekha?prompt=${msg}`);
-  
-  const json = await res.json();
-  
-  
-  let reply = json.result.response;
-    m.reply(reply);
-
-};
-
-handler.help = ['bot'];
+export default handler
 handler.tags = ['fun'];
 handler.command = ['bot', 'alexa'];
 
