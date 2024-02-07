86e704a9635db86e46111beab34be073db1c72ca
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-07 16:24:43 +0100

    Update fun-listas.js

--- plugins/fun-listas.js
+++ plugins/fun-listas.js
@@ -1,8 +1,22 @@
 // By https://github.com/elrebelde21
 
-const handler = async (m, {conn, text}) => {
-  m.reply(`💬• *${pickRandom(global.lista)}*`);
+const handler = async (m, { conn, text }) => {
+  // Extraer el tiempo de la cadena de texto
+  const timeRegex = /(\d{1,2}:\d{2}) (\d{1,2}:\d{2})/;
+  const matches = text.match(timeRegex);
+
+  let replyMessage = `💬• *${pickRandom(global.lista)}*`;
+
+  // Si se encuentra el tiempo en el mensaje
+  if (matches && matches.length >= 3) {
+    const startTime = matches[1];
+    const endTime = matches[2];
+    replyMessage += `\n\nHorario: ${startTime} - ${endTime}`;
+  }
+
+  m.reply(replyMessage);
 };
+
 handler.tags = ['frases'];
 handler.command = ['lista'];
 export default handler;
