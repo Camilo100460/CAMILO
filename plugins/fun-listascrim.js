3e6298b345b842c03937b5e065ad0962f16e36d0
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-07 18:07:52 +0100

    Update fun-listascrim.js

--- plugins/fun-listascrim.js
+++ plugins/fun-listascrim.js
@@ -1,19 +1,51 @@
-// By https://github.com/elrebelde21
-
 const handler = async (m, {conn, text}) => {
-  m.reply(`💬• *${pickRandom(global.lista)}*`);
+  let customMessage = '';
+  let horarios = '';
+  
+  // Verifica si se proporcionó texto junto con el comando
+  if (text) {
+    // Dividir el texto proporcionado en mensaje y horarios
+    const parts = text.split('HORARIOS:');
+    
+    if (parts.length > 1) {
+      customMessage = parts[1].trim(); // Se cambia para que se tome lo que hay despues de HORARIOS:
+      horarios = 'HORARIOS: ' + parts[0].trim();
+    } else {
+      customMessage = text.trim();
+    }
+  }
+  
+  // Mensaje predeterminado de la lista global
+  const randomMessage = pickRandom(global.lista);
+  
+  // Insertar el mensaje personalizado dentro del mensaje predeterminado
+  const finalMessage = `${randomMessage.replace("Aqui quiero que vaya el mensaje personalizado", customMessage)}`;
+  
+  // Responder con el mensaje final
+  m.reply(`${finalMessage}`);
 };
-handler.tags = ['frases'];
-handler.command = ['lista'];
+
+// Etiquetas que describen la función del manejador
+handler.tags = ['mensajes'];
+
+// Comandos que activan este manejador
+handler.command = ['listascrim'];
+
+// Exporta el manejador para su uso en otros módulos
 export default handler;
 
+// Función para seleccionar un elemento aleatorio de una lista
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
-global.lista = ['Mensaje 1', 'Mensaje 2', 'Mensaje 3', 'Mensaje 4', 'Mensaje 5'];
-
-
-
+// Lista global de mensajes
+global.lista = [
+  '_*LISTA DE SCRIM*_ 🦅💙\n\n⏰*HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE SCRIM*_ 🐉💚\n\n⏰*HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n\n       *PRIMER ESCUADRA* 🐉\n\n🏆\n💚 \n💚 \n💚 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE SCRIM*_ 🌍💙\n\n⏰*HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n\n       *PRIMER ESCUADRA* 🌍\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE SCRIM*_ 📖❤️\n\n⏰*HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n\n       *PRIMER ESCUADRA* 📖\n\n🏆\n❤️ \n❤️ \n❤️ \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE SCRIM*_ 📈💙\n\n⏰*HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n\n       *PRIMER ESCUADRA* 📈\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.'
+];
 
 
