0de20cf3587ab41d843be29f22f2a366ad1b91f7
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-07 16:41:18 +0100

    Update fun-listas.js

--- plugins/fun-listas.js
+++ plugins/fun-listas.js
@@ -1,33 +1,29 @@
 // By https://github.com/elrebelde21
 
-const handler = async (m, { conn, text }) => {
-  // Extraer el tiempo de la cadena de texto
-  const timeRegex = /(\d{1,2}:\d{2}) (\d{1,2}:\d{2})/;
-  const matches = text.match(timeRegex);
-
-  let replyMessage = `💬• *${pickRandom(global.lista)}*`;
-
-  // Si se encuentra el tiempo en el mensaje
-  if (matches && matches.length >= 3) {
-    const startTime = matches[1];
-    const endTime = matches[2];
-    replyMessage += `\n\nHorario: ${startTime} - ${endTime}`;
+const handler = async (m, {conn, text}) => {
+  // Verifica si se proporcionó texto junto con el comando
+  if (text) {
+    // Concatena el texto proporcionado con un mensaje aleatorio de la lista global
+    m.reply(`💬• "${text}" ${pickRandom(global.lista)}`);
+  } else {
+    // Si no se proporcionó texto, solo envía un mensaje aleatorio de la lista global
+    m.reply(`💬• *${pickRandom(global.lista)}*`);
   }
-
-  m.reply(replyMessage);
 };
 
-handler.tags = ['frases'];
+// Etiquetas que describen la función del manejador
+handler.tags = ['mensajes'];
+
+// Comandos que activan este manejador
 handler.command = ['lista'];
+
+// Exporta el manejador para su uso en otros módulos
 export default handler;
 
+// Función para seleccionar un elemento aleatorio de una lista
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
+// Lista global de mensajes
 global.lista = ['Mensaje 1', 'Mensaje 2', 'Mensaje 3', 'Mensaje 4', 'Mensaje 5'];
-
-
-
-
-
