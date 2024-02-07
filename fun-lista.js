9dabb51d3eea564c1008e7a3d84f36c1ca9e8d25
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-07 16:07:39 +0100

    Update fun-lista.js

--- fun-lista.js
+++ fun-lista.js
@@ -8,11 +8,10 @@ handler.command = ['lista'];
 export default handler;
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())];
+  return list[Math.floor(Math.random() * list.length)];
 }
 
-global.lista = ['Mensaje 1', 'Mensaje 2', 'Mensaje 3', 'Mensaje 4', 'Mensaje 5', 'Mensaje 6', 'Mensaje 7', 'Mensaje 8', 'Mensaje 9', 'Mensaje 10', 'Mensaje 11', 'Mensaje 12', 'Mensaje 13', 'Mensaje 14', 'Mensaje 15', 'Mensaje 16', 'Mensaje 17', 'Mensaje 18', 'Mensaje 19', 'Mensaje 20'];
-
+global.lista = ['Mensaje 1', 'Mensaje 2', 'Mensaje 3', 'Mensaje 4', 'Mensaje 5'];
 
 
 
