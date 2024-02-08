a02a08a90952219b5a4d5cd404819c7cb9682368
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-08 22:06:20 +0100

    Update menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -1,60 +1,20 @@
-import { clockString } from './clockString.js'; // Asegúrate de importar todas las dependencias necesarias
+let handler = async (m, { conn, text }) => {
+  m.reply(`╭┄┄┄┄┄〔 *Menu Freefire* 〕┄⊱\n┊\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*╰━━━⊰ 𓃠 ${conn.getName(m.sender)} ⊱━━━━დ*`);
+};
 
-import fetch from 'node-fetch';
-
-const handler = async (m, { conn, usedPrefix, __dirname, text }) => {
-  if (!/^\.freefire$/i.test(text)) return; // Se activa solo cuando se escribe ".freefire"
-  try {
-    const pp = imagen4;
-    const img = './Menu2.jpg';
-    const d = new Date(new Date + 3600000);
-    const locale = 'es-ES';
-    const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
-    const _uptime = process.uptime() * 1000;
-    const uptime = clockString(_uptime);
-    const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, limit, level, role} = global.db.data.users[m.sender];
-    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
-    const rtotal = Object.entries(global.db.data.users).length || '0'
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
-
- 📌 *Hola,* ${taguser}
-
-  _*< MENU FREE FIRE 🕹️ />*_
-
-  🧮 _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-  🎭 _${usedPrefix}fake *<txt1> <@tag> <txt2>*_
-  ✊ _${usedPrefix}ppt *<papel / tijera /piedra>*_
-  👨‍💼 _${usedPrefix}prostituto *<nombre / @tag>*_
-  👩‍💼 _${usedPrefix}prostituta *<nombre / @tag>*_
-  🌈 _${usedPrefix}gay2 *<nombre / @tag>*_
-  🌈 _${usedPrefix}lesbiana *<nombre / @tag>*_
-  🐒 _${usedPrefix}pajero *<nombre / @tag>*_
-  🐵 _${usedPrefix}pajera *<nombre / @tag>*_
-  🎉 _${usedPrefix}puto *<nombre / @tag>*_
-  🎉 _${usedPrefix}puta *<nombre / @tag>*_
-  🎮 _${usedPrefix}trabaja *<nombre / @tag>*_
-  📩 _${usedPrefix}anunciar *<txt>*_
-  🔄 _${usedPrefix}tradu *<leng / lenguaje> <txt>*_
-
- ${readMore}
-`.trim()
-    await conn.sendFile(m.chat, pp, 'Menu2.jpg', str, m)
-  } catch (e) {
-    m.reply(util.format(e))
-    console.log(e)
-  }
-}
-
-handler.help = ['freefire']
-handler.tags = ['game']
-handler.command = /^freefire$/i
+handler.tags = ['humor'];
+handler.command = ['Frefire'];
 
 export default handler;
+
+function pickRandom(list) {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  const chosenChiste = list[randomIndex];
+  return chosenChiste.split("\n").map(line => `┊ ${line}`).join("\n");
+}
+
+global.chiste = [
+  "¿Cuál es el último animal que subió al arca de Noé? El del-fin..",
+  "¿Cómo se llama el primo de Bruce Lee? Broco Lee.",
+  "Una madre le dice a su hijo: Jaimito, me ha dicho un pajarito que te drogas. La que te drogas eres tú, que hablas con pajaritos."
+];
