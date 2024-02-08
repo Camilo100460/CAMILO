e66c67f3b7acd5b5f1400a585dccb4b30a358e79
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-08 22:11:03 +0100

    Update menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -1,31 +1,25 @@
+// Handler
 let handler = async (m, { conn, text }) => {
-  m.reply(`╭┄┄┄┄┄〔 *Menu Freefire* 〕┄⊱\n┊\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*╰━━━⊰ 𓃠 ${conn.getName(m.sender)} ⊱━━━━დ*`);
+  if (text.toLowerCase() === ".freefire") {
+    m.reply(`
+_*< 🤖 SOBRE EL BOT 📚 />*_
+
+📜 _${usedPrefix}terminosycondiciones_
+👥 _${usedPrefix}grupos_
+📊 _${usedPrefix}estado_
+📝 _${usedPrefix}infobot_
+🚀 _${usedPrefix}speedtest_
+💸 _${usedPrefix}donar_
+👑 _${usedPrefix}owner_
+💻 _${usedPrefix}script_
+📝 _${usedPrefix}reporte *<txt>*_
+🚪 _${usedPrefix}join *<wagp_url>*_
+🔄 _${usedPrefix}fixmsgespera_
+🤖 _bot_ (sin prefijo)`);
+  }
 };
 
 handler.tags = ['Free fire'];
-handler.command = ['Frefire'];
+handler.command = ['freefire'];
 
 export default handler;
-
-function pickRandom(list) {
-  const randomIndex = Math.floor(Math.random() * list.length);
-  const chosenChiste = list[randomIndex];
-  return chosenChiste.split("\n").map(line => `┊ ${line}`).join("\n");
-}
-
-global.chiste = [
-  " _*< 🤖 SOBRE EL BOT 📚 />*_
-
-  📜 _${usedPrefix}terminosycondiciones_
-  👥 _${usedPrefix}grupos_
-  📊 _${usedPrefix}estado_
-  📝 _${usedPrefix}infobot_
-  🚀 _${usedPrefix}speedtest_
-  💸 _${usedPrefix}donar_
-  👑 _${usedPrefix}owner_
-  💻 _${usedPrefix}script_
-  📝 _${usedPrefix}reporte *<txt>*_
-  🚪 _${usedPrefix}join *<wagp_url>*_
-  🔄 _${usedPrefix}fixmsgespera_
-  🤖 _bot_ (sin prefijo)"
-];
