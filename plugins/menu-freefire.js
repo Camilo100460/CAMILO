626b6b3e5c9b7c0d42e1e8e11be9686376a42165
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-08 22:19:04 +0100

    Update menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -1,7 +1,9 @@
-// Handler
-let handler = async (m, { conn, text, usedPrefix }) => {
-  if (text.toLowerCase() === ".freefire") {
-    m.reply(`
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import fs from 'fs';
+
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const name = await conn.getName(m.sender);
+  const donar =`
 _*< 🤖 SOBRE EL BOT 📚 />*_
 
 📜 _${usedPrefix}terminosycondiciones_
@@ -15,11 +17,13 @@ _*< 🤖 SOBRE EL BOT 📚 />*_
 📝 _${usedPrefix}reporte *<txt>*_
 🚪 _${usedPrefix}join *<wagp_url>*_
 🔄 _${usedPrefix}fixmsgespera_
-🤖 _bot_ (sin prefijo)`);
-  }
+🤖 _bot_ (sin prefijo)
+`.trim();
+  const aa = { quoted: m, userJid: conn.user.jid };
+  const res = generateWAMessageFromContent(m.chat, { liveLocationMessage: { degreesLatitude: 0, degreesLongitude: 0, caption: donar, secuenceNumber: '0', contextInfo: { mentionedJid: conn.parseMention() } } }, aa);
+  conn.relayMessage(m.chat, res.message, {});
 };
 
-handler.tags = ['Free fire'];
-handler.command = ['freefire'];
-
-export default handler;
+handler.help = ['donasi'];
+handler.tags = ['info'];
+handler.command = /\.freefire|\.Free fire|\.free fire$/i;
