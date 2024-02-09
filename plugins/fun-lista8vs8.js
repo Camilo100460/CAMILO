8291e76f74b1ded8d9908f83da65b19964668a04
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-09 01:15:58 +0100

    Update fun-lista8vs8.js

--- plugins/fun-lista8vs8.js
+++ plugins/fun-lista8vs8.js
@@ -1,35 +1,35 @@
 const handler = async (m, {conn, text}) => {
-  let customMessage = '';
-  let horarios = '';
-  
   // Verifica si se proporcionó texto junto con el comando
   if (text) {
-    // Dividir el texto proporcionado en mensaje y horarios
-    const parts = text.split('HORARIOS:');
+    // Dividir el texto proporcionado en hora, color y reglas de los líderes
+    const parts = text.split('/');
     
-    if (parts.length > 1) {
-      customMessage = parts[1].trim(); // Se cambia para que se tome lo que hay despues de HORARIOS:
-      horarios = 'HORARIOS: ' + parts[0].trim();
+    if (parts.length === 3) {
+      const hora = parts[0].trim(); // Se extrae la hora
+      const color = parts[1].trim(); // Se extrae el color
+      const reglasLideres = parts[2].trim(); // Se extraen las reglas de los líderes
+
+      // Mensaje predeterminado de la lista global
+      const randomMessage = pickRandom(global.listaaaaaa);
+      
+      // Insertar la hora, el color y las reglas de los líderes dentro del mensaje predeterminado
+      const finalMessage = `${randomMessage.replace("Aqui quiero que vaya el mensaje personalizado", hora).replace("*COLOR*:", "*COLOR*: " + color).replace("*REGLAS*:", "*REGLAS*: " + reglasLideres)}`;
+      
+      // Responder con el mensaje final
+      m.reply(`${finalMessage}`);
     } else {
-      customMessage = text.trim();
+      m.reply('Por favor, envía tu mensaje en el formato ".prueba1 hora/color/reglas líderes".');
     }
+  } else {
+    m.reply('Por favor, envía tu mensaje en el formato ".prueba1 hora/color/reglas líderes".');
   }
-  
-  // Mensaje predeterminado de la lista global
-  const randomMessage = pickRandom(global.listaaa);
-  
-  // Insertar el mensaje personalizado dentro del mensaje predeterminado
-  const finalMessage = `${randomMessage.replace("Aqui quiero que vaya el mensaje personalizado", customMessage)}`;
-  
-  // Responder con el mensaje final
-  m.reply(`${finalMessage}`);
 };
 
 // Etiquetas que describen la función del manejador
 handler.tags = ['freefire'];
 
 // Comandos que activan este manejador
-handler.command = ['lista8vs8'];
+handler.command = ['lista12vs12'];
 
 // Exporta el manejador para su uso en otros módulos
 export default handler;
@@ -40,15 +40,13 @@ function pickRandom(list) {
 }
 
 // Lista global de mensajes
-global.listaaa = [
-  '_*LISTA DE VS 8VS8*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR:*\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
-  '_*LISTA DE VS 8VS8*_ 🐉💚\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR:*\n\n       *PRIMER ESCUADRA* 🐉\n\n🏆\n💚 \n💚 \n💚\n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
-  '_*LISTA DE VS 8VS8*_ 🌍💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR:*\n\n       *PRIMER ESCUADRA* 🌍\n\n🏆\n💙 \n💙 \n💙\n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
-  '_*LISTA DE VS 8VS8*_ 📖❤️\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR:*\n\n       *PRIMER ESCUADRA* 📖\n\n🏆\n❤️ \n❤️ \n❤️\n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
-  '_*LISTA DE VS 8VS8*_ 📈💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR:*\n\n       *PRIMER ESCUADRA* 📈\n\n🏆\n💙 \n💙 \n💙\n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD'
+global.listaaaaaa = [
+  '_*LISTA DE VS 12VS12*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE VS 12VS12*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE VS 12VS12*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE VS 12VS12*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE VS 12VS12*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙\n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
 ];
-
-
 
 
 
