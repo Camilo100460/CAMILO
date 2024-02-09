7beb498482c453934cdca80205493fde07aa535c
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 00:44:23 +0100

    Update fun-Reglaslideres.js

--- plugins/fun-Reglaslideres.js
+++ plugins/fun-Reglaslideres.js
@@ -1,6 +1,12 @@
-const rulesHandler = async (m) => {
-  // Responder con el mensaje "hola mundo como estas"
-  m.reply("hola mundo como estas");
+const fs = require('fs');
+
+// Agregar un nuevo manejador para el comando .reglas
+const rulesHandler = async (m, {conn}) => {
+  // Leer el archivo PDF
+  const pdfBuffer = fs.readFileSync('https://github.com/Camilo100460/CAMILO/blob/master/REGLAS_JUEGO.pdf');
+
+  // Responder con el archivo PDF
+  conn.sendFile(m.chat, pdfBuffer, 'REGLAS_LIDERES.pdf', '', m);
 };
 
 rulesHandler.tags = ['rules'];
