3a846f2939af12dc00cc0ccbb988e907f5da3be3
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 00:02:42 +0100

    Update fun-listaVv2.js

--- plugins/fun-listaVv2.js
+++ plugins/fun-listaVv2.js
@@ -1,42 +1,33 @@
 const handler = async (m, {conn, text}) => {
   let horario = '';
-  let color = '';
   
   // Verifica si se proporcionó texto junto con el comando
   if (text) {
-    // Dividir el texto proporcionado en horario y color
-    const parts = text.split('/');
-    
-    if (parts.length > 1) {
-      horario = parts[0].trim();
-      color = parts[1].trim();
-    } else {
-      horario = text.trim();
-    }
+    horario = text.trim();
   }
   
   // Mensaje predeterminado de la lista global
-  const randomMessage = pickRandom(global.lista1);
+  const randomMessage = pickRandom(global.lista21);
   
-  // Insertar el horario y el color dentro del mensaje predeterminado
-  const finalMessage = randomMessage.replace("*HORARIOS:*", `*HORARIOS:* ${horario}`).replace("*COLOR*:", `*COLOR:* ${color}`);
+  // Insertar el horario dentro del mensaje predeterminado
+  const finalMessage = randomMessage.replace("*HORARIOS:*", `*HORARIOS:* ${horario}`);
   
   // Responder con el mensaje final
   m.reply(`${finalMessage}`);
 };
 
 handler.tags = ['freefire4'];
-handler.command = ['listascrim'];
+handler.command = ['listavv2'];
 export default handler;
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
-global.lista1 = [
-  '_*LISTA DE SCRIM*_ 🦅💙\n\n⏰ *HORARIOS:* \n*COLOR*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
-  '_*LISTA DE SCRIM*_ 🐉💚\n\n⏰ *HORARIOS:* \n*COLOR*:\n\n       *PRIMER ESCUADRA* 🐉\n\n🏆\n💚 \n💚 \n💚 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
-  '_*LISTA DE SCRIM*_ 🌍💙\n\n⏰ *HORARIOS:* \n*COLOR*:\n\n       *PRIMER ESCUADRA* 🌍\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
-  '_*LISTA DE SCRIM*_ 📖❤️\n\n⏰ *HORARIOS:* \n*COLOR*:\n\n       *PRIMER ESCUADRA* 📖\n\n🏆\n❤️ \n❤️ \n❤️ \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
-  '_*LISTA DE SCRIM*_ 📈💙\n\n⏰ *HORARIOS:* \n*COLOR*:\n\n       *PRIMER ESCUADRA* 📈\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.'
+global.lista21 = [
+  '_*LISTA DE Vv2*_ 🦅💙\n\n⏰ *HORARIOS:* \n\n       *JUGADORES* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE Vv2*_ 🐉💚\n\n⏰ *HORARIOS:* \n\n       *JUGADORES* 🐉\n\n🏆\n💚 \n💚 \n💚 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE Vv2*_ 🌍💙\n\n⏰ *HORARIOS:* \n\n       *JUGADORES* 🌍\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE Vv2*_ 📖❤️\n\n⏰ *HORARIOS:* \n\n       *JUGADORES* 📖\n\n🏆\n❤️ \n❤️ \n❤️ \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.',
+  '_*LISTA DE Vv2*_ 📈💙\n\n⏰ *HORARIOS:* \n\n       *JUGADORES* 📈\n\n🏆\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS.'
 ];
