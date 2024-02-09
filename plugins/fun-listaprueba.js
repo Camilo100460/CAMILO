52680407bf5ef63cc01f393d2cfe31f83473330b
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-09 01:27:19 +0100

    Update fun-listaprueba.js

--- plugins/fun-listaprueba.js
+++ plugins/fun-listaprueba.js
@@ -1,34 +1,49 @@
-let color = 'NEGRO'; // Variable para almacenar el color
+const handler = async (m, {conn, text}) => {
+  // Verifica si se proporcionó texto junto con el comando
+  if (text) {
+    // Dividir el texto proporcionado en hora, color y reglas de los líderes
+    const parts = text.split('/');
+    
+    if (parts.length === 3) {
+      const hora = parts[0].trim(); // Se extrae la hora
+      const color = parts[1].trim(); // Se extrae el color
+      const reglasLideres = parts[2].trim(); // Se extraen las reglas de los líderes
 
-const handler = async (m, {conn, text, usedPrefix}) => {
-  // Si el color no se ha establecido, preguntar por el color
-  if (!color) {
-    m.reply('Por favor, dime qué color te gustaría para el mensaje general.');
-    return;
+      // Mensaje predeterminado de la lista global
+      const randomMessage = pickRandom(global.listaaaaaaa);
+      
+      // Insertar la hora, el color y las reglas de los líderes dentro del mensaje predeterminado
+      const finalMessage = `${randomMessage.replace("Aqui quiero que vaya el mensaje personalizado", hora).replace("*COLOR*:", "*COLOR*: " + color).replace("*REGLAS*:", "*REGLAS*: " + reglasLideres)}`;
+      
+      // Responder con el mensaje final
+      m.reply(`${finalMessage}`);
+    } else {
+      m.reply('Por favor, envía tu mensaje en el formato ".prueba1 hora/color/reglas líderes".');
+    }
+  } else {
+    m.reply('Por favor, envía tu mensaje en el formato ".prueba1 hora/color/reglas líderes".');
   }
-
-  // Mensaje predeterminado con la lista de comandos
-  const defaultMessage = `
-  _*< 📜 LISTA DE MENUS 📋 />*_
-
-  COLOR: ${color}
-  HORA:
-  
-  🎵 _${usedPrefix}(lista)_
-  📺 _${usedPrefix}lista (8vs8)_
-  📖 _${usedPrefix}lista (12vs12)_
-  📖 _${usedPrefix}lista (16vs16)_
-  `;
-  
-  // Responder con el mensaje final
-  m.reply(defaultMessage);
 };
 
 // Etiquetas que describen la función del manejador
-handler.tags = ['freefi22re1'];
+handler.tags = ['freefire'];
 
 // Comandos que activan este manejador
-handler.command = ['prueba'];
+handler.command = ['lista12vs12'];
 
 // Exporta el manejador para su uso en otros módulos
 export default handler;
+
+// Función para seleccionar un elemento aleatorio de una lista
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+// Lista global de mensajes
+global.listaaaaaaa = [
+  '_*LISTA DE CUADRILATERO*_ 🦅💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *TERCER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE CUADRILATERO*_ 🐉💚\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🐉\n\n🏆\n💚 \n💚 \n💚 \n\n       *TERCER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE CUADRILATERO*_ 🌍💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 🌍\n\n🏆\n💙 \n💙 \n💙 \n\n       *TERCER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE CUADRILATERO*_ 📖❤️\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 📖\n\n🏆\n❤️ \n❤️ \n❤️ \n\n       *TERCER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+  '_*LISTA DE CUADRILATERO*_ 📈💙\n\n⏰ *HORARIOS:* Aqui quiero que vaya el mensaje personalizado\n*COLOR*:\n*REGLAS*:\n\n       *PRIMER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\n       *SEGUNDA ESCUADRA* 📈\n\n🏆\n💙 \n💙 \n💙 \n\n       *TERCER ESCUADRA* 🦅\n\n👑\n💙 \n💙 \n💙 \n\nMUCHA RESPONSABILIDAD CON LOS HORARIOS. 15 MINUTOS ANTES EN DISCORD',
+];
