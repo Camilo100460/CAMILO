814710d6bb142578098c09ef8da979dd364ed06b
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 03:55:48 +0100

    Update fun-Reglaslideres.js

--- plugins/fun-Reglaslideres.js
+++ plugins/fun-Reglaslideres.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
-
-// Agregar un nuevo manejador para el comando .reglas
-const rulesHandler = async (m, {conn}) => {
-  // Leer el archivo PDF
-  const pdfBuffer = fs.readFileSync('C:/may/CAMILO/REGLAS_JUEGO.pdf');
-
-  // Responder con el archivo PDF
-  conn.sendFile(m.chat, pdfBuffer, 'REGLAS_LIDERES.pdf', '', m);
+const handler = async (m, {conn, usedPrefix}) => {
+  try {
+    const pp = imagen2;
+    
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp}, {quoted: fkontak2});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+  }
 };
-
-rulesHandler.tags = ['rules'];
-rulesHandler.command = ['reglas'];
-export default rulesHandler;
+handler.command = /^(reglas|reglasprueba)$/i;
+export default handler;
