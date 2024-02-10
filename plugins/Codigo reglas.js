5b1e78636890182ab5bbdc946527407503af373b
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 23:50:24 +0100

    Update Codigo reglas.js

--- plugins/Codigo reglas.js
+++ plugins/Codigo reglas.js
@@ -1,18 +1,18 @@
 const handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sInfi = text; // Guarda el texto personalizado en la base de datos
-    m.reply('*[❗] Mensaje de bienvenida configurado correctamente para Infi.*');
+    global.db.data.chats[m.chat].sReglasinfi = text; // Guarda el texto personalizado en la base de datos
+    m.reply('*[❗] Mensaje de configurado correctamente.*');
   } else {
-    const sInfi = global.db.data.chats[m.chat].sInfi; // Obtiene el texto personalizado de la base de datos
+    const sInfi = global.db.data.chats[m.chat].sReglasinfi; // Obtiene el texto personalizado de la base de datos
     if (sInfi) {
       m.reply(sInfi); // Envía el mensaje personalizado si está configurado
     } else {
-      m.reply('*[❗] No se ha configurado un mensaje de bienvenida para Infi.*');
+      m.reply('*[❗] No se ha configurado un mensaje.*');
     }
   }
 };
 
-handler.help = ['.infi <texto>', '.infi'];
+handler.help = ['.Reglasinfi <texto>', '.Reglasinfi'];
 handler.tags = ['group'];
 handler.command = ['infi'];
 handler.admin = true;
