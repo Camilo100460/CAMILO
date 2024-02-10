e58d0608e2bc924c533ca2790edb6ccf820f4788
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 23:52:48 +0100

    Update Codigo reglas.js

--- plugins/Codigo reglas.js
+++ plugins/Codigo reglas.js
@@ -1,20 +1,20 @@
 const handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sReglasinfi = text; // Guarda el texto personalizado en la base de datos
-    m.reply('*[❗] Mensaje de configurado correctamente.*');
+    global.db.data.chats[m.chat].sPrueba = text; // Guarda el texto personalizado en la base de datos
+    m.reply('*[❗] Mensaje de bienvenida configurado correctamente para Prueba.*');
   } else {
-    const sInfi = global.db.data.chats[m.chat].sReglasinfi; // Obtiene el texto personalizado de la base de datos
-    if (sInfi) {
-      m.reply(sInfi); // Envía el mensaje personalizado si está configurado
+    const sPrueba = global.db.data.chats[m.chat].sPrueba; // Obtiene el texto personalizado de la base de datos
+    if (sPrueba) {
+      m.reply(sPrueba); // Envía el mensaje personalizado si está configurado
     } else {
-      m.reply('*[❗] No se ha configurado un mensaje.*');
+      m.reply('*[❗] No se ha configurado un mensaje de bienvenida para Prueba.*');
     }
   }
 };
 
-handler.help = ['.Reglasinfi <texto>', '.Reglasinfi'];
+handler.help = ['.prueba <texto>', '.prueba'];
 handler.tags = ['group'];
-handler.command = ['Reglasinfi'];
+handler.command = ['prueba'];
 handler.admin = true;
 
 export default handler;
