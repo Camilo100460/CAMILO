a8a0037eb64ff9081911cfdeb89691b045208f91
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-11 00:11:59 +0100

    Update Reglas Compe.js

--- plugins/Reglas Compe.js
+++ plugins/Reglas Compe.js
@@ -1,4 +1,18 @@
+const cooldowns = {};
+
 const handler = async (m, { conn, text, isROwner, isOwner }) => {
+  const userId = m.sender; // Obtiene el ID del remitente del mensaje
+  
+  // Verifica si el usuario está en el registro de enfriamiento y si ha pasado el tiempo de enfriamiento
+  if (cooldowns[userId] && cooldowns[userId] > Date.now()) {
+    const remainingTime = (cooldowns[userId] - Date.now()) / 1000;
+    m.reply(`Debes esperar ${remainingTime.toFixed(1)} segundos antes de volver a usar este comando.`);
+    return;
+  }
+  
+  // Registra el momento actual más el tiempo de enfriamiento (por ejemplo, 10 segundos)
+  cooldowns[userId] = Date.now() + 10000; // 10 segundos de enfriamiento
+  
   if (text) {
     global.db.data.chats[m.chat].sReglascompe = text; // Guarda el texto personalizado en la base de datos
     m.reply('*[❗] Mensaje de bienvenida configurado correctamente para Reglascompe.*');
