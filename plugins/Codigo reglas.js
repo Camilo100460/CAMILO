d1752316d0378566770e5f460e59a71a1c5421f0
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 22:49:36 +0100

    Update Codigo reglas.js

--- plugins/Codigo reglas.js
+++ plugins/Codigo reglas.js
@@ -5,54 +5,30 @@ let mensajes = {};
 const handler = async (m, {conn, usedPrefix, text}) => {
   if (usedPrefix == '.infi') {
     if (text) {
-      // Si hay texto después del comando, lo guardamos
       mensajes[m.chat] = text;
       conn.reply(m.chat, 'Mensaje guardado.', m);
     } else {
-      // Si no hay texto después del comando, enviamos el mensaje guardado
       let mensaje = mensajes[m.chat];
-      if (mensaje) {
-        conn.reply(m.chat, 'Mensaje guardado: ' + mensaje, m);
-      } else {
-        conn.reply(m.chat, 'No hay ningún mensaje guardado.', m);
-      }
+      conn.reply(m.chat, mensaje ? 'Mensaje guardado: ' + mensaje : 'No hay ningún mensaje guardado.', m);
     }
   }
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    // Comentamos las líneas donde se envía la imagen
-    // const pp = imagen4;
-    // const img = './Menu2.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';
-    const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const {money, joincount, exp, limit, level, role} = user;
     const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
     const rtotal = Object.entries(global.db.data.users).length || '0'
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
  📌 *Hola,* ${taguser}
  📊 _*< TUS ESTADÍSTICAS />*_
  🖼️ _${usedPrefix}viewimage_`.trim();
-    if (m.isGroup) {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      // conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
-      conn.reply(m.chat, str.trim(), m);
-    } else {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      // conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
-      conn.reply(m.chat, str.trim(), m);
-    }
+    conn.reply(m.chat, str.trim(), m);
   } catch {
     conn.reply(m.chat, '*[ ℹ️ ] Este menu tiene un error interno, por lo cual no fue posible enviarlo.*', m);
   }
