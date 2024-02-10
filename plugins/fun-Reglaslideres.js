216c9af96a919251328389626759e09f6901e5fe
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 04:12:58 +0100

    Update fun-Reglaslideres.js

--- plugins/fun-Reglaslideres.js
+++ plugins/fun-Reglaslideres.js
@@ -9,8 +9,8 @@ const handler = async (m, { conn }) => {
       return;
     }
 
-    const pdfBuffer = fs.readFileSync(pdfPath); // Lee el contenido del archivo PDF
-    const pdfName = path.basename(pdfPath); // Obtén el nombre del archivo PDF
+    const pdfBuffer = fs.readFileSync(pdfPath); // Leer el contenido del archivo PDF
+    const pdfName = path.basename(pdfPath); // Obtener el nombre del archivo PDF
 
     const fkontak2 = {
       'key': {
@@ -29,11 +29,11 @@ const handler = async (m, { conn }) => {
     const messageOptions = {
       quoted: fkontak2,
       mimetype: 'application/pdf',
-      filename: pdfName, // Usa el nombre del archivo como nombre de archivo
-      caption: pdfName // Agrega el nombre del archivo como texto del mensaje
+      caption: pdfName // Agregar el nombre del archivo como texto del mensaje
     };
 
-    conn.sendMessage(m.chat, { document: pdfBuffer }, messageOptions);
+    // Enviar el documento con el nombre del archivo como texto del mensaje
+    await conn.sendMessage(m.chat, { document: pdfBuffer, mimetype: 'application/pdf' }, messageOptions);
   } catch (error) {
     console.error(error);
     conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
