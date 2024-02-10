4c44523339bc9fc5207a30d956b561e2b8fc9071
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 23:19:36 +0100

    Update Codigo reglas.js

--- plugins/Codigo reglas.js
+++ plugins/Codigo reglas.js
@@ -1,20 +1,32 @@
 const handler = async (m, { conn, text, isROwner, isOwner }) => {
-  if (text) {
-    global.db.data.chats[m.chat].sInfi = text; // Guarda el texto personalizado en la base de datos
-    m.reply('*[❗] Mensaje configurado correctamente.*');
-  } else {
-    const sInfi = global.db.data.chats[m.chat].sInfi; // Obtiene el texto personalizado de la base de datos
-    if (sInfi) {
-      m.reply(sInfi); // Envía el mensaje personalizado si está configurado
-    } else {
-      m.reply('*[❗] No se ha configurado un mensaje.*');
-    }
+  const chatId = m.chat;
+  const [command, restOfText] = text.split(" ", 2);
+
+  switch (command) {
+    case '.infi1':
+    case '.infi2':
+    case '.infi3':
+      const index = parseInt(command.replace(".infi", "")) - 1;
+      if (restOfText) {
+        global.db.data.chats[chatId][`sInfi${index + 1}`] = restOfText.trim();
+        m.reply(`*[❗] Mensaje ${index + 1} configurado correctamente para Infi.*`);
+      } else {
+        const sInfi = global.db.data.chats[chatId][`sInfi${index + 1}`];
+        if (sInfi) {
+          m.reply(sInfi);
+        } else {
+          m.reply(`*[❗] No se ha configurado un mensaje ${index + 1} para Infi.*`);
+        }
+      }
+      break;
+    default:
+      m.reply('*[❗] El comando no es válido. Use .infi1, .infi2 o .infi3 seguido del mensaje.*');
   }
 };
 
-handler.help = ['.Mensaje <texto>', '.Mensaje'];
+handler.help = ['.infi1 <texto>', '.infi2 <texto>', '.infi3 <texto>', '.infi1', '.infi2', '.infi3'];
 handler.tags = ['group'];
-handler.command = ['Mensaje'];
+handler.command = ['infi1', 'infi2', 'infi3'];
 handler.admin = true;
 
 export default handler;
