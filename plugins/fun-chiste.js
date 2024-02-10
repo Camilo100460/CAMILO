732c87fe643d2475229942fa3203a00acd41186d
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 02:19:31 +0100

    Update fun-chiste.js

--- plugins/fun-chiste.js
+++ plugins/fun-chiste.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  m.reply(`╭┄┄┄┄┄〔 *Chiste* 〕┄⊱\n┊\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*╰━━━⊰ 𓃠 ${conn.getName(m.sender)} ⊱━━━━დ*`);
+  m.reply(`\n *😹 ${pickRandom(global.chiste)} 😹*\n┊\n*      𓃠 ${conn.getName(m.sender)}*`);
 };
 
 handler.tags = ['humor'];
