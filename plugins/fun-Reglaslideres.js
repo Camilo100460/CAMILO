e13f29580be43e5c9f75f6b264bb06fff53bd9a4
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-10 04:16:05 +0100

    Update fun-Reglaslideres.js

--- plugins/fun-Reglaslideres.js
+++ plugins/fun-Reglaslideres.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 
 const handler = async (m, { conn }) => {
   try {
@@ -10,32 +9,11 @@ const handler = async (m, { conn }) => {
       return;
     }
 
-    const pdfName = path.basename(pdfPath); // Obtener el nombre del archivo PDF
-
-    const fkontak2 = {
-      'key': {
-        'fromMe': false,
-        'participant': '0@s.whatsapp.net',
-        'remoteJid': 'status@broadcast',
-        'id': 'Halo'
-      },
-      'message': {
-        'contactMessage': {
-          'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-        }
-      }
-    };
-
-    const messageOptions = {
-      quoted: fkontak2,
-      caption: pdfName // Agregar el nombre del archivo como texto del mensaje
-    };
-
     // Leer el archivo como un búfer
     const pdfBuffer = fs.readFileSync(pdfPath);
 
-    // Enviar el archivo como documento adjunto con el nombre correcto
-    await conn.sendFile(m.chat, pdfBuffer, pdfName, messageOptions);
+    // Enviar el archivo como documento adjunto sin especificar el nombre del archivo
+    await conn.sendFile(m.chat, pdfBuffer, '', { quoted: m });
   } catch (error) {
     console.error(error);
     conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
