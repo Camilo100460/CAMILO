f3e54c23c82df8a88130ace510d150b6fad202a8
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-11 01:20:43 +0100

    Update Reglas Infi.js

--- plugins/Reglas Infi.js
+++ plugins/Reglas Infi.js
@@ -15,13 +15,13 @@ const handler = async (m, { conn, text, isROwner, isOwner }) => {
   
   if (text) {
     global.db.data.chats[m.chat].sReglasinfi = text; // Guarda el texto personalizado en la base de datos
-    m.reply('*[❗] Mensaje de bienvenida configurado correctamente para Reglasinfi.*');
+    m.reply('*[❗] Mensaje configurado correctamente para Reglasinfi.*');
   } else {
     const sReglasinfi = global.db.data.chats[m.chat].sReglasinfi; // Obtiene el texto personalizado de la base de datos
     if (sReglasinfi) {
       m.reply(sReglasinfi); // Envía el mensaje personalizado si está configurado
     } else {
-      m.reply('*[❗] No se ha configurado un mensaje de bienvenida para Reglasinfi.*');
+      m.reply('*[❗] No se ha configurado un mensaje para Reglasinfi.*');
     }
   }
 };
