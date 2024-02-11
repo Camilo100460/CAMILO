2fcb2f55448b4007244111efca9dfeef8fc1aa35
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-11 01:20:09 +0100

    Update Reglas Compe.js

--- plugins/Reglas Compe.js
+++ plugins/Reglas Compe.js
@@ -15,13 +15,13 @@ const handler = async (m, { conn, text, isROwner, isOwner }) => {
   
   if (text) {
     global.db.data.chats[m.chat].sReglascompe = text; // Guarda el texto personalizado en la base de datos
-    m.reply('*[❗] Mensaje de bienvenida configurado correctamente para Reglascompe.*');
+    m.reply('*[❗] Mensaje se configurado correctamente para Reglascompe.*');
   } else {
     const sReglascompe = global.db.data.chats[m.chat].sReglascompe; // Obtiene el texto personalizado de la base de datos
     if (sReglascompe) {
       m.reply(sReglascompe); // Envía el mensaje personalizado si está configurado
     } else {
-      m.reply('*[❗] No se ha configurado un mensaje de bienvenida para Reglascompe.*');
+      m.reply('*[❗] No se ha configurado un mensaje para Reglascompe.*');
     }
   }
 };
