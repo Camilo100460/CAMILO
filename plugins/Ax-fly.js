28eb9077a1047899806244dd171b584a2382fd10
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 04:11:02 +0100

    Update Ax-fly.js

--- plugins/Ax-fly.js
+++ plugins/Ax-fly.js
@@ -1,25 +1,26 @@
-import { lenguajeGB } from './ruta/a/lenguajeGB'; // Importa la variable lenguajeGB si es necesaria
 import db from './ruta/a/db'; // Importa la base de datos si es necesaria
 
-const handler = async (m, { conn, text, isROwner, isOwner }) => {
+const handler = async (m, { conn, text }) => {
   const userId = m.sender; // Obtiene el ID del remitente del mensaje
-  let sDiscord = db.data.chats[m.chat].sDiscord; // Obtiene el texto personalizado de la base de datos
-  if (sDiscord) {
-    sDiscord = sDiscord.replace('$url', 'https://tu-url-personalizada.com'); // Reemplaza $url con la URL personalizada
-    conn.reply(m.chat, sDiscord, m); // Envía el mensaje personalizado con la URL actualizada
-  } else {
-    conn.reply(m.chat, '*[❗] No se ha configurado el mensaje para Discord.*', m);
-  }
-  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`;
-  let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
-  conn.sendFile(m.chat, url, 'error.jpg', `May te desea mucha suerte`, m);
-  // conn.sendButton(m.chat, `May te desea mucha suerte.`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m);
-}
 
-handler.help = ['reglasx-fly'];
+  if (text) {
+    db.data.chats[m.chat].sDiscord = text; // Guarda la URL proporcionada en la base de datos
+    m.reply('*[❗] Información actualizada correctamente para Discord.*');
+  } else {
+    const sDiscord = db.data.chats[m.chat].sDiscord; // Obtiene la URL personalizada de la base de datos
+    if (sDiscord) {
+      conn.sendFile(m.chat, sDiscord, 'error.jpg', `Aquí está la imagen personalizada para Discord`, m);
+    } else {
+      m.reply('*[❗] No se ha configurado ninguna URL personalizada para Discord.*');
+    }
+  }
+};
+
+handler.help = ['reglasx-fly <URL>', 'reglasx-fly'];
 handler.tags = ['internet'];
-handler.command = /^(reglasx-fly)$/;
-handler.exp = 50;
-handler.level = 0;
+handler.command = /^(reglasx-fly)$/i;
+handler.group = true;
+
+export default handler;
 
 export default handler;
