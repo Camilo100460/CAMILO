13b2b3d07e6d41b68c1c23a195c37284cec21091
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 01:54:46 +0100

    Update Ax-fly.js

--- plugins/Ax-fly.js
+++ plugins/Ax-fly.js
@@ -1,13 +1,16 @@
 let handler = async (m, { conn, command }) => {
     if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
-    let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
-    conn.sendFile(m.chat, url, 'error.jpg', `🥵 ♥ PIES ♥  🥵`, m)
+    
+    // Aquí puedes cambiar la URL por la que desees usar
+    let url = "URL_PERSONALIZADA"; // Cambia "URL_PERSONALIZADA" por la URL que deseas utilizar
+    
+    conn.sendFile(m.chat, url, 'imagen.jpg', `May te desea mucha suerte`, m)
     //conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 
-handler.help = ['x-fly']
+handler.help = ['reglas-fly']
 handler.tags = ['internet']
-handler.command = /^(x-fly)$/
+handler.command = /^(reglasx-fly)$/
 handler.exp = 50
 handler.level = 0
 
