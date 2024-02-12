2b1369732d1c496512f482f7f979c8103e39ea14
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 19:31:57 +0100

    Update Ax-fly (2).js

--- plugins/Ax-fly (2).js
+++ plugins/Ax-fly (2).js
@@ -1,30 +1,30 @@
-let handler = async (m, { conn, text, usedPrefix }) => {
-    if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+const handler = async (m, { conn, text }) => {
+    // Comprobamos si el comando es .reglasx-fly
+    if (text && /^.reglasx-fly/i.test(text)) {
+        const url = text.split(' ')[1]; // Extraemos la URL de la imagen de la entrada del usuario
+        
+        if (url) {
+            global.db.data.urls[m.chat] = url; // Guardamos la URL en la base de datos
+            m.reply('URL de imagen guardada con éxito.');
+            return;
+        } else {
+            m.reply('Por favor, proporciona una URL de imagen después de `.reglasx-fly`.');
+            return;
+        }
+    }
     
-    let url = text // La URL de la imagen se proporciona después del comando
+    // Si el comando no es .reglasx-fly, se busca la URL guardada
+    const url = global.db.data.urls[m.chat];
     if (url) {
-        // Si se proporciona una URL, guardarla en la base de datos
-        db.data.urls[m.chat] = url
-        conn.reply(m.chat, 'URL de imagen guardada con éxito.', m)
-        db.save()
-        return
+        conn.sendFile(m.chat, url, 'imagen.jpg', `Mensaje de ejemplo`, m);
+    } else {
+        m.reply('No se ha configurado ninguna URL de imagen.');
     }
+};
 
-    // Si no se proporciona una URL, buscar en la base de datos la URL guardada
-    url = db.data.urls[m.chat]
-    if (!url) {
-        // Si no hay ninguna URL guardada, enviar un mensaje indicando que no hay URL guardada
-        return conn.reply(m.chat, `No se ha guardado ninguna URL de imagen. Para guardar una URL, usa ${usedPrefix}reglasx-fly <URL>.`, m)
-    }
+handler.help = ['.reglasx-fly <URL>', '.reglasx-fly'];
+handler.tags = ['internet'];
+handler.command = ['reglasx-fly'];
+handler.admin = true;
 
-    // Envía la imagen utilizando la URL guardada
-    conn.sendFile(m.chat, url, 'imagen.jpg', `Mensaje de ejemplo`, m)
-}
-
-handler.help = ['reglasx-fly']
-handler.tags = ['internet']
-handler.command = /^(reglasx-fly)$/i
-handler.exp = 50
-handler.level = 0
-
-module.exports = handler
+export default handler;
