c29c707d50c9b1bd41c3fbf6f387d68a486a2e6a
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 04:43:15 +0100

    Update Ax-fly (2).js

--- plugins/Ax-fly (2).js
+++ plugins/Ax-fly (2).js
@@ -1,11 +1,24 @@
-let handler = async (m, { conn, command }) => {
-    if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
-    let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
+let handler = async (m, { conn, command, text }) => {
+    if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB'smsContAdult'}`
+    
+    // Si se proporciona un texto, se asume que es una URL y se guarda en la base de datos
+    if (text) {
+        db.data.chats[m.chat].customUrl = text;
+        m.reply('*[❗] URL configurada correctamente.*');
+        return;
+    }
+    
+    // Si no se proporciona un texto, se recupera la URL de la base de datos
+    let url = db.data.chats[m.chat].customUrl;
+    if (!url) {
+        m.reply('*[❗] No se ha configurado ninguna URL.*');
+        return;
+    }
+    
     conn.sendFile(m.chat, url, 'error.jpg', `May te desea mucha suerte`, m)
-    //conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 
-handler.help = ['reglas-fly']
+handler.help = ['reglas-fly <url>', 'reglas-fly']
 handler.tags = ['internet']
 handler.command = /^(reglasx-fly)$/
 handler.exp = 50
