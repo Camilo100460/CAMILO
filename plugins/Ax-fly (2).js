e0f0896e7001daa62f2ee7cabc28a3b661469d63
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 19:36:12 +0100

    Update Ax-fly (2).js

--- plugins/Ax-fly (2).js
+++ plugins/Ax-fly (2).js
@@ -1,29 +1,21 @@
-const handler = async (m, { conn, text }) => {
-    // Comprobamos si el comando es .reglasx-fly
-    if (text && /^.reglasx-fly/i.test(text)) {
-        const url = text.split(' ')[1]; // Extraemos la URL de la imagen de la entrada del usuario
-        
-        if (url) {
-            global.db.data.urls[m.chat] = url; // Guardamos la URL en la base de datos
-            m.reply('URL de imagen guardada con éxito.');
-            return;
-        } else {
-            m.reply('Por favor, proporciona una URL de imagen después de `.reglasx-fly`.');
-            return;
-        }
-    }
-    
-    // Si el comando no es .reglasx-fly, se busca la URL guardada
-    const url = global.db.data.urls[m.chat];
-    if (url) {
-        conn.sendFile(m.chat, url, 'imagen.jpg', `Mensaje de ejemplo`, m);
+const handler = async (m, { conn, text, isROwner, isOwner }) => {
+  global.db.data.chats[m.chat] = global.db.data.chats[m.chat] || {}; // Inicializa global.db.data.chats[m.chat] si es undefined
+  
+  if (text) {
+    global.db.data.chats[m.chat].sReglasxFly = text; // Guarda el texto personalizado en la base de datos
+    m.reply('*[❗] Mensaje se configurado correctamente para Reglasx-fly.*');
+  } else {
+    const sReglasxFly = global.db.data.chats[m.chat].sReglasxFly; // Obtiene el texto personalizado de la base de datos
+    if (sReglasxFly) {
+      m.reply(sReglasxFly); // Envía el mensaje personalizado si está configurado
     } else {
-        m.reply('No se ha configurado ninguna URL de imagen.');
+      m.reply('*[❗] No se ha configurado un mensaje para Reglasx-fly.*');
     }
+  }
 };
 
-handler.help = ['.reglasx-fly <URL>', '.reglasx-fly'];
-handler.tags = ['internet'];
+handler.help = ['.reglasx-fly <texto>', '.reglasx-fly'];
+handler.tags = ['group'];
 handler.command = ['reglasx-fly'];
 handler.admin = true;
 
