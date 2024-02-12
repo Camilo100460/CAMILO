ac7d7e122deb364fac32e30220e7b27a23688e0a
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 04:12:33 +0100

    Update Ax-fly.js

--- plugins/Ax-fly.js
+++ plugins/Ax-fly.js
@@ -1,15 +1,16 @@
 import db from './ruta/a/db'; // Importa la base de datos si es necesaria
 
 const handler = async (m, { conn, text }) => {
-  const userId = m.sender; // Obtiene el ID del remitente del mensaje
-
+  const chatId = m.chat; // Obtiene el ID del chat
+  
   if (text) {
-    db.data.chats[m.chat].sDiscord = text; // Guarda la URL proporcionada en la base de datos
+    // Sobrescribe la URL proporcionada en la base de datos
+    db.data.chats[chatId].sDiscord = text.trim(); // Guarda la URL proporcionada en la base de datos
     m.reply('*[❗] Información actualizada correctamente para Discord.*');
   } else {
-    const sDiscord = db.data.chats[m.chat].sDiscord; // Obtiene la URL personalizada de la base de datos
+    const sDiscord = db.data.chats[chatId].sDiscord; // Obtiene la URL personalizada de la base de datos
     if (sDiscord) {
-      conn.sendFile(m.chat, sDiscord, 'error.jpg', `Aquí está la imagen personalizada para Discord`, m);
+      conn.sendFile(chatId, sDiscord, 'error.jpg', `Aquí está la imagen personalizada para Discord`, m);
     } else {
       m.reply('*[❗] No se ha configurado ninguna URL personalizada para Discord.*');
     }
@@ -20,7 +21,5 @@ handler.help = ['reglasx-fly <URL>', 'reglasx-fly'];
 handler.tags = ['internet'];
 handler.command = /^(reglasx-fly)$/i;
 handler.group = true;
-
-export default handler;
 
 export default handler;
