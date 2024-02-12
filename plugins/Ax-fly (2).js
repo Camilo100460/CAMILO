f268466a90323249e0e2dc8e47a8e2f3c1006ecd
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 19:29:41 +0100

    Update Ax-fly (2).js

--- plugins/Ax-fly (2).js
+++ plugins/Ax-fly (2).js
@@ -1,13 +1,23 @@
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, usedPrefix }) => {
     if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
     
     let url = text // La URL de la imagen se proporciona después del comando
-    if (!url) {
-        // Si no se proporciona una URL, se envía un mensaje indicando cómo usar el comando
-        return conn.reply(m.chat, 'Por favor, proporciona una URL de imagen después del comando.', m)
+    if (url) {
+        // Si se proporciona una URL, guardarla en la base de datos
+        db.data.urls[m.chat] = url
+        conn.reply(m.chat, 'URL de imagen guardada con éxito.', m)
+        db.save()
+        return
     }
-    
-    // Envía la imagen utilizando la URL proporcionada
+
+    // Si no se proporciona una URL, buscar en la base de datos la URL guardada
+    url = db.data.urls[m.chat]
+    if (!url) {
+        // Si no hay ninguna URL guardada, enviar un mensaje indicando que no hay URL guardada
+        return conn.reply(m.chat, `No se ha guardado ninguna URL de imagen. Para guardar una URL, usa ${usedPrefix}reglasx-fly <URL>.`, m)
+    }
+
+    // Envía la imagen utilizando la URL guardada
     conn.sendFile(m.chat, url, 'imagen.jpg', `Mensaje de ejemplo`, m)
 }
 
@@ -17,4 +27,4 @@ handler.command = /^(reglasx-fly)$/i
 handler.exp = 50
 handler.level = 0
 
-export default handler
+module.exports = handler
