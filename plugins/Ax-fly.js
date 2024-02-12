1b2edbbe914e9a317cefe01f04b8e0dd9d457052
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 01:37:17 +0100

    Update Ax-fly.js

--- plugins/Ax-fly.js
+++ plugins/Ax-fly.js
@@ -1,19 +1,13 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn, command }) => {
-    if (command === 'hola') { // Si el comando es .hola
-        conn.sendText(m.chat, '¡Hola! ¿Cómo estás?') // Envía este mensaje
-    } else { // Para cualquier otro comando
-        if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
-        let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
-        conn.sendFile(m.chat, url, 'error.jpg', `🥵 ♥ PIES ♥  🥵`, m)
-        //conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
-    }
+    if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+    let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
+    conn.sendFile(m.chat, url, 'error.jpg', `🥵 ♥ PIES ♥  🥵`, m)
+    //conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 
-handler.help = ['x-fly', 'hola'] // Agrega el comando .hola a la lista de ayuda
+handler.help = ['x-fly']
 handler.tags = ['internet']
-handler.command = /^(x-fly|hola)$/ // Ahora el comando acepta tanto .x-fly como .hola
+handler.command = /^(x-fly)$/
 handler.exp = 50
 handler.level = 0
 
