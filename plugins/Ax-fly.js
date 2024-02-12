1e7252a6a8c5c212e93ff105ee84e6085fa5e174
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-12 03:33:56 +0100

    Update Ax-fly.js

--- plugins/Ax-fly.js
+++ plugins/Ax-fly.js
@@ -2,13 +2,14 @@ let handler = async (m, { conn, command }) => {
     if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
     let url = "https://i.ibb.co/8DpxWFc/ENFRENTAMIENTOS.jpg"; // Cambia este enlace por el que desees usar
     conn.sendFile(m.chat, url, 'error.jpg', `May te desea mucha suerte`, m)
-    //conn.sendButton(m.chat, `May te desea mucha suerte.`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+    //conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 
-handler.help = ['fly']
+handler.help = ['reglas-fly']
 handler.tags = ['internet']
-handler.command = /^(fly)$/
+handler.command = /^(reglasx-fly)$/
 handler.exp = 50
 handler.level = 0
 
 export default handler
+
