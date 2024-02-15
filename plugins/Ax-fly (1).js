34e1b796c9e1a78d0e392fe27aaa7e863b76028e
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-16 00:17:56 +0100

    Update Ax-fly (1).js

--- plugins/Ax-fly (1).js
+++ plugins/Ax-fly (1).js
@@ -1,5 +1,7 @@
 let lastUsed = 0;
 
+let iqList = ['1', '14', '23', '35', '41', '50', '67', '72', '86', '99', '150', '340', '423', '500', '676', '780', '812', '945', '1000', '¡Ilimitado!', '5000', '7500', '10000'];
+
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙀𝙎𝘾𝙍𝙄𝘽𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙋𝘼𝙍𝘼 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙍 𝙎𝙐 𝘾𝙊𝘾𝙄𝙀𝙉𝙏𝙊 𝙌𝙐𝙊𝘾𝙄𝙀𝙉𝙏𝙄𝙁𝙄𝘾𝙊`
 
@@ -15,11 +17,19 @@ let handler = async (m, { conn, command, text }) => {
     // Actualizar el tiempo del último uso
     lastUsed = currentTime;
 
-    let iq = `_🧠 *${text}* tu coeficiente intelectual es de *${Math.floor(Math.random() * 200)}* 👩🏻‍🎓_ 
-    `.trim()
+    let iq = iqList[Math.floor(Math.random() * iqList.length)];
+    let message = '';
+
+    if (iq <= 50) {
+        message = `_🧠 *${text}*, tu coeficiente intelectual es de *${iq}*. ¡No te preocupes, todos tenemos días malos! 👩🏻‍🎓_`;
+    } else if (iq >= 1000) {
+        message = `_🧠 *${text}*, tu coeficiente intelectual es de *${iq}*. ¡Eres un genio! 👩🏻‍🎓_`;
+    } else {
+        message = `_🧠 *${text}*, tu coeficiente intelectual es de *${iq}*. ¡Buen trabajo! 👩🏻‍🎓_`;
+    }
 
     // Enviar la respuesta del "shippeo"
-    m.reply(iq, null, { mentions: conn.parseMention(iq) })
+    m.reply(message, null, { mentions: conn.parseMention(message) })
 }
 
 handler.help = ['iqtest']
