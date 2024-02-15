c2af4441b08c52d6ff7ea2f75f3052e45b1e82b4
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-16 00:12:23 +0100

    Update Ax-fly (1).js

--- plugins/Ax-fly (1).js
+++ plugins/Ax-fly (1).js
@@ -2,11 +2,6 @@ let lastUsed = 0;
 
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙀𝙎𝘾𝙍𝙄𝘽𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙋𝘼𝙍𝘼 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙍 𝙎𝙐 𝘾𝙊𝘾𝙄𝙀𝙉𝙏𝙊 𝙌𝙐𝙊𝘾𝙄𝙀𝙉𝙏𝙄𝙁𝙄𝘾𝙊`
-    
-    let [text1, ...text2] = text.split(' ')
-    text2 = (text2 || []).join(' ')
-    
-    if (!text2) throw `⚠️ 𝙁𝘼𝙇𝙏𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙀𝙂𝙐𝙉𝘿𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼`
 
     // Obtener el tiempo actual en milisegundos
     let currentTime = Date.now();
@@ -20,7 +15,7 @@ let handler = async (m, { conn, command, text }) => {
     // Actualizar el tiempo del último uso
     lastUsed = currentTime;
 
-    let iq = `_🧠 *${text1}* tu coeficiente intelectual es de *${Math.floor(Math.random() * 200)}* 👩🏻‍🎓_ 
+    let iq = `_🧠 *${text}* tu coeficiente intelectual es de *${Math.floor(Math.random() * 200)}* 👩🏻‍🎓_ 
     `.trim()
 
     // Enviar la respuesta del "shippeo"
