837a2bb30b96bc92132220f3bab44878f0b06050
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-17 01:20:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('The May\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Camilo Daza`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
