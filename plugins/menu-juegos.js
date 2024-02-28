a3afe20d8e9fb8208d249fc181d9d87c00312df0
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-28 17:05:51 +0100

    Update menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -25,45 +25,53 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 
 const document = doc[Math.floor(Math.random() * doc.length)];
- const str = `╭━━━━･❪ *DIVERSIONES* ❫ ･━━━━╮
-┃🔥 _!ruleta_
-┃🔥 _!mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-┃🔥 _!fake *<texto1> <@tag> <texto2>*_
-┃🔥 _!ppt *<papel / tijera /piedra>*_
-┃🔥 _!prostituto *<nombre / @tag>*_
-┃🔥 _!prostituta *<nombre / @tag>*_
-┃🔥 _!gay2 *<nombre / @tag>*_
-┃🔥 _!lesbiana *<nombre / @tag>*_
-┃🔥 _!pajero *<nombre / @tag>*_
-┃🔥 _!pajera *<nombre / @tag>*_
-┃🔥 _!follar *<nombre / @tag>*_
-┃🔥 _!puto *<nombre / @tag>*_
-┃🔥 _!puta *<nombre / @tag>*_
-┃🔥 _!manco *<nombre / @tag>*_
-┃🔥 _!manca *<nombre / @tag>*_
-┃🔥 _!rata *<nombre / @tag>*_
-┃🔥 _!love *<nombre / @tag>*_
-┃🔥 _!doxear *<nombre / @tag>*_
-┃🔥 _!pregunta *<texto>*_
-┃🔥 _!suitpvp *<@tag>*_
-┃🔥 _!slot *<apuesta>*_
-┃🔥 _!ttt *<nombre sala>*_
-┃🔥 _!delttt_
-┃🔥 _!acertijo_
-┃🔥 _!personalidad *<nombre / @tag>*_
-┃🔥 _!simi *<texto>*_
-┃🔥 _!top *<texto>*_
-┃🔥 _!topgays_
-┃🔥 _!topotakus_
-┃🔥 _!formarpareja_
-┃🔥 _!verdad_
-┃🔥 _!crime_
-┃🔥 _!reto_
-┃🔥 _!cancion_
-┃🔥 _!pista_
-┃🔥 _!akinator_
-┃🔥 _!wordfind_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•`.trim();     if (m.isGroup) { 
+ const str = `
+🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
+    
+📌 *Hola,* @${m.sender.split('@')[0]}
+
+╭━━━━━━ ◜◌◝ ━━━━━━╮
+│ _*< LISTA DE COMANDOS />*_
+│
+├ • 🏹 _${usedPrefix}ruleta_
+├ • 📝 _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+├ • 📚 _${usedPrefix}fake *<texto1> <@tag> <texto2>*_
+├ • 📖 _${usedPrefix}ppt *<papel / tijera /piedra>*_
+├ • 🧮 _${usedPrefix}prostituto *<nombre / @tag>*_
+├ • 🏔️ _${usedPrefix}prostituta *<nombre / @tag>*_
+├ • 🌊 _${usedPrefix}gay2 *<nombre / @tag>*_
+├ • 🐿️ _${usedPrefix}lesbiana *<nombre / @tag>*_
+├ • 🧸 _${usedPrefix}pajero *<nombre / @tag>*_
+├ • 🍄 _${usedPrefix}pajera *<nombre / @tag>*_
+├ • ✒️ _${usedPrefix}!follar *<nombre / @tag>*_
+├ • 🍂 _${usedPrefix}puto *<nombre / @tag>*_
+├ • 📺 _${usedPrefix}puta *<nombre / @tag>*_
+├ • 🍪 _${usedPrefix}manco *<nombre / @tag>*_
+├ • 🚀 _${usedPrefix}manca *<nombre / @tag>*_
+├ • ⚡ _${usedPrefix}rata *<nombre / @tag>*_
+├ • 🌟 _${usedPrefix}love *<nombre / @tag>*_
+├ • 💥 _${usedPrefix}doxear *<nombre / @tag>*_
+├ • 🎈 _${usedPrefix}pregunta *<texto>*_
+├ • 🎁 _${usedPrefix}suitpvp *<@tag>*_
+├ • 🎮 _${usedPrefix}slot *<apuesta>*_
+├ • 🕹️ _${usedPrefix}ttt *<nombre sala>*_
+├ • 🎲 _${usedPrefix}delttt_
+├ • 🧩 _${usedPrefix}acertijo_
+├ • 🧠 _${usedPrefix}personalidad *<nombre / @tag>*_
+├ • 🎭 _${usedPrefix}simi *<texto>*_
+├ • 🧐 _${usedPrefix}top *<texto>*_
+├ • 🕶️ _${usedPrefix}topgays_
+├ • 🎓 _${usedPrefix}topotakus_
+├ • 🤵 _${usedPrefix}formarpareja_
+├ • 🎩 _${usedPrefix}verdad_
+├ • 🧙‍♂️ _${usedPrefix}crime_
+├ • 🕵️ _${usedPrefix}reto_
+├ • 🧗 _${usedPrefix}cancion_
+├ • 🏞️ _${usedPrefix}pista_
+├ • 🗿 _${usedPrefix}akinator_
+├ • 🧩 _${usedPrefix}wordfind_
+│
+╰━━━━━━ ◟◌◞ ━━━━━━╯`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
