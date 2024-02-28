d3a36a58f3bda17bc7a48a1d2ceda81dbab2d1b1
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-28 17:52:39 +0100

    Update rpg-cazar.js

--- plugins/rpg-cazar.js
+++ plugins/rpg-cazar.js
@@ -2,10 +2,10 @@
 const handler = async (m, { conn, usedPrefix, command, groupMetadata, participants, isPrems }) => {
     const date = global.db.data.users[m.sender].monster_hunt + 3600000; // 3600000 = 1 hora
     if (new Date - global.db.data.users[m.sender].monster_hunt < 3600000) {
-        return m.reply(`Debes esperar un poco más para volver a cazar monstruos. Tiempo restante: ${msToTime(date - new Date())}`);
+        return m.reply(`*La Reina May te convocó para cazar a un enorme gigante que amenaza nuestras tierras. Aventura próxima:* ${msToTime(date - new Date())}`);
     }
 
-    const monsters = ['Goblin', 'Ogro', 'Esqueleto', 'Dragón', 'Demonio'];
+    const monsters = ['Duende Maligno', 'Ogro Despiadado', 'Esqueleto Aterrador', 'Dragón Devastador', 'Demonio Infernal', 'Rey de los Duendes', 'Reina de las Hadas', 'Hidra Devoradora', 'Minotauro Furioso', 'Cíclope Colosal', 'Quimera Monstruosa', 'Vampiro Inmortal', 'Licántropo Salvaje', 'Fantasma Espeluznante', 'Espectro Siniestro', 'Bruja Malévola', 'Dragón de Fuego', 'Gigante Titánico', 'Kraken Devorador', 'Fénix Resplandeciente'];
     const monster = monsters[Math.floor(Math.random() * monsters.length)];
     const exp = Math.floor(Math.random() * 1000) + 500; // Experiencia entre 500 y 1500
     const diamonds = Math.floor(Math.random() * 5) + 1; // Diamantes entre 1 y 5
@@ -16,20 +16,20 @@ const handler = async (m, { conn, usedPrefix, command, groupMetadata, participan
     global.db.data.users[m.sender].monster_hunt = new Date * 1;
 
     if (success) {
-        conn.reply(m.chat, `¡Has cazado a un ${monster} y has ganado ${exp} de experiencia, ${diamonds} diamantes y ${coins} monedas!`, m);
+        conn.reply(m.chat, `*¡Has cazado a un ${monster} y has ganado ${exp} de experiencia, ${diamonds} diamantes y ${coins} monedas!*`, m);
         global.db.data.users[m.sender].exp += exp;
         global.db.data.users[m.sender].diamonds += diamonds;
         global.db.data.users[m.sender].coins += coins;
     } else {
         const lostExp = Math.floor(Math.random() * 200) + 100; // Pérdida de experiencia entre 100 y 300
-        conn.reply(m.chat, `¡Intentaste cazar a un ${monster}, pero fue demasiado fuerte y perdiste ${lostExp} de experiencia!`, m);
+        conn.reply(m.chat, `*¡Intentaste cazar a un ${monster}, pero fue demasiado fuerte y perdiste ${lostExp} de experiencia!*`, m);
         global.db.data.users[m.sender].exp -= lostExp;
     }
 }
 
 handler.help = ['cazar'];
 handler.tags = ['xp'];
-handler.command = /^(moun)$/i;
+handler.command = /^(monstruos)$/i;
 handler.register = true;
 handler.group = true;
 
