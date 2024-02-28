daaaa8e133cca84051a861ed43a79b6cbd9f34ea
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-28 18:22:07 +0100

    Update rpg-cazar.js

--- plugins/rpg-cazar.js
+++ plugins/rpg-cazar.js
@@ -1,7 +1,7 @@
 const handler = async (m, { conn, usedPrefix, command, groupMetadata, participants, isPrems }) => {
     const date = global.db.data.users[m.sender].monster_hunt + 3600000; // 3600000 = 1 hora
     if (new Date - global.db.data.users[m.sender].monster_hunt < 3600000) {
-        return m.reply(`👑 *La Reina May te convocó para cazar a un enorme gigante que amenaza nuestras tierras./nAventura próxima:* ${msToTime(date - new Date())}`);
+        return m.reply(`🏹 *La Reina May te convocó para cazar a un enorme gigante que amenaza nuestras tierras.*/n/n ⏰ • *Aventura próxima:* ${msToTime(date - new Date())}`);
     }
 
     const monsters = [
