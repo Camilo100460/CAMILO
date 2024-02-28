4054d9ad90aaee07e6e12885c92e33cbd000a8d0
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-28 16:05:51 +0100

    Update pruebaconvertidor-toimg.js

--- plugins/pruebaconvertidor-toimg.js
+++ plugins/pruebaconvertidor-toimg.js
@@ -1,18 +1,18 @@
-import { webp2png } from '../lib/webp2mp4.js'
-
-let handler = async (m, { conn, usedPrefix, command, mid }) => {
-    const notStickerMessage = `Por favor, responde a un sticker con el comando ${usedPrefix + command}`
-    if (!m.quoted) throw notStickerMessage
-    const q = m.quoted || m
-    let mime = q.mediaType || ''
-    if (!/image\/webp/.test(mime)) throw notStickerMessage
-    let media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'imagen.png', null, m)
+let { webp2png } = require('../lib/webp2mp4')
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  let mime = m.quoted.mimetype || ''
+  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  let media = await m.quoted.download()
+  let out = Buffer.alloc(0)
+  if (/webp/.test(mime)) {
+    out = await webp2png(media)
+  }
+  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+    thumbnail: Buffer.alloc(0)
+  })
 }
-
-handler.help = ['toimg (responder)']
+handler.help = ['toimg (reply)']
 handler.tags = ['sticker']
-handler.command = ['toimg2', 'img2', 'jpg2']
-
-export default handler
+handler.command = ['toimg2']
+module.exports = handler
