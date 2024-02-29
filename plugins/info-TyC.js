012fff7e8538822d71d9c14473a4c5839c70e9c5
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-02-29 23:57:28 +0100

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -6,24 +6,15 @@ handler.command = new RegExp;
 export default handler;
 
 global.terminos = `
-*=> El desconocimiento de lo que aqui se menciona no exime al propietario del Bot, Sub Bot o usuario del bot de la sanciones que se puedan derivar❗*
-
-*⚠️ Términos de privacidad*
-
-_1.- La información que reciba el Bot NO se comparte con ningún tercero ni con nadie_
-_2.- Las imágenes/videos/sticker/audios/etc no son compartidos con nadie_
-_3.- Su numero de teléfono no es compartido con absolutamente nadie_
-_4.- Datos de tarjetas, ubicaciones, direcciones, etc son eliminados inmediatamente y no se comparten con absolutamente nadie_
-_5.- Todas las conversaciones son eliminadas cada determinado tiempo y no se hace backup (no se guarda) de ningún tipo de información/conversación_
+*=> El desconocimiento de lo que aqui se menciona exime al propietario del Bot, Sub Bot o usuario del bot de la sanciones que se puedan derivar❗*
 
 *⚠️ Términos de uso*
 
 _1.- No nos hacemos responsables de un mal uso que se le pueda dar al Bot_
 _2.- No nos hacemos responsables de la ignorancia y/o desconocimiento del tema_
 _3.- El Bot no esta activo las 24 horas al menos de que el propietario decida lo contrario_
-_4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial, ya que son personas (terceros) y no tenemos control sobre ell@s_
-_5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
-_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de May-Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
+_4.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
+_5.- El creador del BOT no se hace responsable de las acciones de terceros que vendan, distribuyan o promocionen la aplicación. Cualquier transacción o interacción con terceros es responsabilidad exclusiva del usuario, y el creador de la aplicación no asume ninguna responsabilidad por cualquier problema_
 
 *⁉️ Te quedaron dudas?*
-_- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_`;
+_- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal +573145699786 al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_`;
