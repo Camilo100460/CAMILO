8332d55850b1eb571ffe7b452cb92673c93ab063
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-01 21:44:35 +0100

    Update Juego de carreras.js

--- plugins/Juego de carreras.js
+++ plugins/Juego de carreras.js
@@ -1,15 +1,70 @@
-const handler = async (m, {conn, text}) => {
-  const aa = {quoted: m, userJid: conn.user.jid};
-  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: `💐• *${pickRandom(global.refran2)}*`, contextInfo: {externalAdReply: {title: '[ ⚠ ] Advertencia', body: '𝙈𝙖𝙮-𝘽𝙤𝙩', thumbnail: imagen1, sourceUrl: 'https://github.com/Anksawhs'}}}}, aa);
-  if (text) return conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id});
+import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
+import os from "os";
+import util from "util";
+import sizeFormatter from "human-readable";
+import MessageType from "@whiskeysockets/baileys";
+import fs from "fs";
+import { performance } from "perf_hooks";
+const handler = async (m, { conn, usedPrefix }) => {
+  const _uptime = process.uptime() * 1000;
+  const uptime = clockString(_uptime);
+  const chats = Object.entries(conn.chats).filter(
+    ([id, data]) => id && data.isChats,
+  );
+  const groupsIn = chats.filter(([id]) => id.endsWith("@g.us"));
+  const groups = chats.filter(([id]) => id.endsWith("@g.us"));
+  const old = performance.now();
+  const neww = performance.now();
+  const rtime = (neww - old).toFixed(7);
+  const wm = 'May-Bot';
+  const info = `           _*TU - CARRERA*_
+
+👥•  _*Propi:*_ MAY Y CAMILO
+📞•  _*WA:*_ +525514278383 +573178454976
+💳•  _*PayPal:*_ www.paypal.me/camildaza  
+https://www.paypal.me/MayGarcia772 
+`.trim();
+  const doc = [
+    "pdf",
+    "zip",
+    "vnd.openxmlformats-officedocument.presentationml.presentation",
+    "vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "vnd.openxmlformats-officedocument.wordprocessingml.document",
+  ];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const Message = {
+    document: { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
+    mimetype: `application/${document}`,
+    fileName: `Documento`,
+    fileLength: 99999999999999,
+    pageCount: 200,
+    contextInfo: {
+      forwardingScore: 200,
+      isForwarded: true,
+      externalAdReply: {
+        mediaUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+        mediaType: 2,
+        previewType: "pdf",
+        title: "The Mystic - Bot",
+        body: "Repositorio - GitHub",
+        thumbnail: imagen1,
+        sourceUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+      },
+    },
+    caption: info,
+    footer: wm,
+    headerType: 6,
+  };
+  conn.sendMessage(m.chat, Message, { quoted: m });
 };
 
-handler.tags = ['frases'];
-handler.command = ['carrera'];
+handler.command = /^(ping|info|status|estado)$/i;
 export default handler;
 
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())];
+function clockString(ms) {
+  const h = Math.floor(ms / 3600000);
+  const m = Math.floor(ms / 60000) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+  console.log({ ms, h, m, s });
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
 }
-
-global.refran2 = ["Más vale estar solo que mal acompañado.","Adios rogando con el mazo dando"]
