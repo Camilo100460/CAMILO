af3ea1b12ac867d5a00241a58bb11a6f790dec8a
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-02 00:26:21 +0100

    Update Juego de carreras.js

--- plugins/Juego de carreras.js
+++ plugins/Juego de carreras.js
@@ -2,9 +2,9 @@ const handler = async (m, { conn, usedPrefix }) => {
   try {
     const pp = imagen2;
     const equipos = {
-      debut: ["Once Caldas (Colombia)", "Boca Juniors (Argentina)"],
-      joven: ["Once Caldas (Colombia)", "Boca Juniors (Argentina)"],
-      maduro: ["FC Porto (Portugal)", "Ajax (Holanda)"],
+      debut: ["Once Caldas (Colombia)", "Boca Juniors (Argentina)", "Atlético Nacional (Colombia)", "América de Cali (Colombia)", "Millonarios (Colombia)", "Independiente Santa Fe (Colombia)", "Deportivo Cali (Colombia)", "Junior (Colombia)", "River Plate (Argentina)", "Independiente (Argentina)", "Racing Club (Argentina)", "San Lorenzo (Argentina)", "Huracán (Argentina)", "Vélez Sarsfield (Argentina)", "Alianza Lima (Perú)", "Universitario de Deportes (Perú)", "Sporting Cristal (Perú)", "Melgar (Perú)", "Alianza Universidad (Perú)", "Cienciano (Perú)", "Barcelona SC (Ecuador)", "Emelec (Ecuador)", "Liga de Quito (Ecuador)", "Independiente del Valle (Ecuador)", "Universidad Católica (Ecuador)", "Delfín SC (Ecuador)"],
+      joven: ["Once Caldas (Colombia)", "Boca Juniors (Argentina)", "Atlético Nacional (Colombia)", "América de Cali (Colombia)", "Millonarios (Colombia)", "Independiente Santa Fe (Colombia)", "Deportivo Cali (Colombia)", "Junior (Colombia)", "River Plate (Argentina)", "Independiente (Argentina)", "Racing Club (Argentina)", "San Lorenzo (Argentina)", "Huracán (Argentina)", "Vélez Sarsfield (Argentina)", "Alianza Lima (Perú)", "Universitario de Deportes (Perú)", "Sporting Cristal (Perú)", "Melgar (Perú)", "Alianza Universidad (Perú)", "Cienciano (Perú)", "Barcelona SC (Ecuador)", "Emelec (Ecuador)", "Liga de Quito (Ecuador)", "Independiente del Valle (Ecuador)", "Universidad Católica (Ecuador)", "Delfín SC (Ecuador)"],
+      maduro: ["FC Porto (Portugal)", "Ajax (Holanda)", "SL Benfica (Portugal)", "Sporting CP (Portugal)", "SC Braga (Portugal)", "Vitória SC (Portugal)", "Belenenses SAD (Portugal)", "Marítimo (Portugal)", "Portimonense (Portugal)", "Boavista (Portugal)", "Famalicão (Portugal)", "Gil Vicente (Portugal)", "Paços de Ferreira (Portugal)", "Ajax Amsterdam (Holanda)", "PSV Eindhoven (Holanda)", "Feyenoord Rotterdam (Holanda)", "AZ Alkmaar (Holanda)", "FC Utrecht (Holanda)", "Vitesse Arnhem (Holanda)", "FC Groningen (Holanda)", "SC Heerenveen (Holanda)", "Heracles Almelo (Holanda)", "FC Twente (Holanda)", "Willem II (Holanda)", "Fortuna Sittard (Holanda)"],
       pico: ["Sevilla FC (España)", "Villarreal CF (España)"],
       veterano: ["Bayern Munich (Germany)", "Paris Saint-Germain (France)", "Juventus (Italia)"],
       retiro: ["Club América (México)", "River Plate (Argentina)"]
@@ -17,8 +17,8 @@ const handler = async (m, { conn, usedPrefix }) => {
     const rojas = Math.floor(Math.random() * 5) + 1; // Valor entre 1 y 5
     const champions = Math.floor(Math.random() * 10) + 1; // Valor entre 1 y 10
     const mundiales = Math.floor(Math.random() * 5) + 1; // Valor entre 1 y 5
-    const goles = Math.floor(Math.random() * 500) + 1; // Valor entre 1 y 500
-    const balonOros = Math.floor(Math.random() * 7) + 1; // Valor entre 1 y 7
+    const goles = Math.floor(Math.random() * 500) + 70; // Valor entre 1 y 500
+    const balonOros = Math.floor(Math.random() * 3) + 1; // Valor entre 1 y 7
 
     const str = `
 📊 _*< TU CARRERA />*_
