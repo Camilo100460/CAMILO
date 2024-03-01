a2b503466b0b482472f08a63461fedbffa029fbe
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-01 21:57:29 +0100

    Update Juego de carreras.js

--- plugins/Juego de carreras.js
+++ plugins/Juego de carreras.js
@@ -1,28 +1,14 @@
 import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
-import os from "os";
-import util from "util";
-import sizeFormatter from "human-readable";
 import MessageType from "@whiskeysockets/baileys";
-import fs from "fs";
 import { performance } from "perf_hooks";
+
 const handler = async (m, { conn, usedPrefix }) => {
-  const _uptime = process.uptime() * 1000;
-  const uptime = clockString(_uptime);
   const chats = Object.entries(conn.chats).filter(
     ([id, data]) => id && data.isChats,
   );
-  const groupsIn = chats.filter(([id]) => id.endsWith("@g.us"));
   const groups = chats.filter(([id]) => id.endsWith("@g.us"));
-  const old = performance.now();
-  const neww = performance.now();
-  const rtime = (neww - old).toFixed(7);
   const wm = 'May-Bot';
   const info = `           _*TU - CARRERA*_
-
-👥•  _*Propi:*_ MAY Y CAMILO
-📞•  _*WA:*_ +525514278383 +573178454976
-💳•  _*PayPal:*_ www.paypal.me/camildaza  
-https://www.paypal.me/MayGarcia772 
 `.trim();
   const doc = [
     "pdf",
@@ -42,13 +28,13 @@ https://www.paypal.me/MayGarcia772
       forwardingScore: 200,
       isForwarded: true,
       externalAdReply: {
-        mediaUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+        mediaUrl: "https://github.com",
         mediaType: 2,
         previewType: "pdf",
-        title: "The Mystic - Bot",
+        title: "May - Bot",
         body: "Repositorio - GitHub",
         thumbnail: imagen1,
-        sourceUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+        sourceUrl: "https://github.com",
       },
     },
     caption: info,
@@ -60,11 +46,3 @@ https://www.paypal.me/MayGarcia772
 
 handler.command = /^(JUEGO)$/i;
 export default handler;
-
-function clockString(ms) {
-  const h = Math.floor(ms / 3600000);
-  const m = Math.floor(ms / 60000) % 60;
-  const s = Math.floor(ms / 1000) % 60;
-  console.log({ ms, h, m, s });
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
-}
