5e790023a2ecd8a08f640bd7cf948bb2b29150cc
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-01 22:46:26 +0100

    Update Juego de carreras.js

--- plugins/Juego de carreras.js
+++ plugins/Juego de carreras.js
@@ -1,25 +1,14 @@
-import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
-import MessageType from "@whiskeysockets/baileys";
-import { performance } from "perf_hooks";
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!text) throw `_*< DESCARGAS - TIKTOK />*_\n\n*[ ℹ️ ] Ingrese un enlace de TikTok.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
+  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `_*DESCARGAS - TIKTOK*_  🎧\n\n*[ ℹ️ ] Ingrese un enlace de TikTok.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
 
-const handler = async (m, { conn, usedPrefix }) => {
-  const chats = Object.entries(conn.chats).filter(
-    ([id, data]) => id && data.isChats,
-  );
-  const groups = chats.filter(([id]) => id.endsWith("@g.us"));
-  const wm = 'May-Bot';
-  const info = `           _*TU - CARRERA*_
+  const desc = `_*DESCARGAS - TIKTOK*_  🎧\n\n*[ ℹ️ ] Tu video está en proceso de envío. Por favor, ten paciencia.*`;
 
-
-`.trim();
-  ];
-  const Message = {
-    mimetype: "text/plain",
-    text: info,
-    footer: wm,
-  };
-  conn.sendMessage(m.chat, Message, { quoted: m });
+  try {
+    const aa = { quoted: m, userJid: conn.user.jid };
+    const prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: desc, contextInfo: { externalAdReply: { title: '𝙈𝙖𝙮-𝘽𝙤𝙩', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD' }, mentionedJid: [m.sender] } } }, aa);
+    await conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] });
+  } catch (ee1) {
+    throw `_*< DESCARGAS - TIKTOK />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
+  }
 };
-
-handler.command = /^(JUEGO)$/i;
-export default handler;
