c223d2d773b81a03359b7f5c4183c53a899990cc
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-02 17:20:24 +0100

    Update info-covid.js

--- plugins/info-covid.js
+++ plugins/info-covid.js
@@ -1,21 +1,29 @@
 import fetch from 'node-fetch';
+
 const handler = async (m, {text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝙿𝙰𝙸𝚂, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} Mexico*`;
-  const res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)));
-  if (!res.ok) throw await res.text();
-  const json = await res.json();
-  if (!json.confirmed) throw 'País?';
-  if (json.confirmed) {
+  try {
+    if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝙿𝙰𝙸𝚂, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} Mexico*`;
+
+    const res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)));
+    if (!res.ok) throw await res.text();
+
+    const json = await res.json();
+    if (!json.confirmed) throw 'País?';
+
     m.reply(`
 🌏 País : ${text}
-✅Confirmado : ${json.confirmed.value}
-📉curado : ${json.recovered.value}
-☠️Muertes : ${json.deaths.value}
-💌Info Actualizada : ${json.lastUpdate}
+✅ Confirmado : ${json.confirmed.value}
+📉 Curado : ${json.recovered.value}
+☠️ Muertes : ${json.deaths.value}
+💌 Info Actualizada : ${json.lastUpdate}
 `.trim());
-  } else throw json;
+  } catch (error) {
+    m.reply(`Ha ocurrido un error: ${error}`);
+  }
 };
+
 handler.help = ['covid'].map((v) => v + ' <país>');
 handler.tags = ['info'];
 handler.command = /^(corona|covid|covid19)$/i;
+
 export default handler;
