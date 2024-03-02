5a6212c9e9e40f26b5eb6d2d0026196fb7b06026
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-02 20:29:59 +0100

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ const handler = async (m, {text}) => {
   const user = global.db.data.users[m.sender];
   user.afk = + new Date;
   user.afkReason = text;
-  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] EL USUARIO\n ${conn.getName(m.sender)} ESTARA INACTIVO (𝙰𝙵𝙺), POR FAVOR NO LO ETIQUETEN*\n\n\n*📝• MOTIVO DE LA INACTIVIDAD (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
+  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] EL USUARIO\n ${conn.getName(m.sender)} ESTARA INACTIVO (𝙰𝙵𝙺), POR FAVOR NO LO ETIQUETEN* \n\n\n*📝• MOTIVO DE LA INACTIVIDAD (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
 `);
 };
 handler.help = ['afk [alasan]'];
