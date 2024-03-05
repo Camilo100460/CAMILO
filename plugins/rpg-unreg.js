fb54dd484dd6fe2c31235284440dfb269138839c
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-05 20:22:25 +0100

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -3,7 +3,7 @@ const handler = async function(m, {args}) {
   if (!args[0]) throw '_*[❗𝐍𝐅𝐎❗] INGRESE SU NUMERO DE SERIE, SI NO LO RECUERDA PUEDE USAR EL COMANDO #myns*_';
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw '*[❗𝐍𝐅𝐎❗] NUMERO DE SERIE INCORRECTO, 𝙲COMPRUEBE QUE LO HAYA ESCRITO CORRECTAMENTE!*\n\n*SI NO LO RECUERDA PUEDE USAR EL COMANDO #myns*';
+  if (args[0] !== sn) throw '*[❗𝐍𝐅𝐎❗] NUMERO DE SERIE INCORRECTO, 𝙲OMPRUEBE QUE LO HAYA ESCRITO CORRECTAMENTE!*\n\n*SI NO LO RECUERDA PUEDE USAR EL COMANDO #myns*';
   user.registered = false;
   m.reply(`*[ ✔ ] SE REALIZO CON EXITO, USTED YA NO ESTA REGISTRADO EN EL BOT*`);
 };
