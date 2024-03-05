d0fb15861fa9013330b9bcdeacc3522994e694c4
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-05 18:11:51 +0100

    Update prueba22.js

--- plugins/prueba22.js
+++ plugins/prueba22.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   try {
     const q = m.quoted ? m.quoted : m || m.text || m.sender;
     const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
-    const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid);
+    const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), 'Hola soy May-Bot', conn.user.jid);
     await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
   } catch {
     const htextos = 'Hola soy May-Bot';
