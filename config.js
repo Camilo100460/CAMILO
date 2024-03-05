197522c9ad8e9c8634205e1681c489593cfacfef
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-05 20:58:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
 global.imagen5 = fs.readFileSync('./src/+18.jpg');
 global.imagen6 = fs.readFileSync('./Menu3.png');
-global.imagen7 = fs.readFileSync('./camilo.jpg');
+global.imagen10 = fs.readFileSync('./camilo.jpg');
 
 global.mods = [];
 
