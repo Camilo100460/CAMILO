4eb39a1820e3727b04ec9ab5c08ab48f2928a92c
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-09 00:27:55 +0100

    Update herramientas-topdf.js

--- plugins/herramientas-topdf.js
+++ plugins/herramientas-topdf.js
@@ -2,7 +2,7 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m, {conn, text, usedPrefix, command, isOwner}) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 / 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*';
+  if (!mime) throw '*[❗] RESPONDA / ETIQUETE A UNA IMAGEN*';
   const img = await q.download?.();
   const url = await uploadImage(img);
   const docname = text ? text : m.pushName || 'documento';
