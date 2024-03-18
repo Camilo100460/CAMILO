32e04a0af41de35d55bc8addb25fcfc776e182a7
Camilo100460 <154703830+Camilo100460@users.noreply.github.com>
2024-03-18 18:46:06 +0100

    Update random-Hentai.js

--- plugins/random-Hentai.js
+++ plugins/random-Hentai.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const res = (await axios.get(`https://raw.githubusercontent.com/Camilo100460/CAMILO/master/src/JSON/hentai.json`)).data;
   const url = await res[Math.floor(res.length * Math.random())];
-  conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m);
+  conn.sendFile(m.chat, url, 'error.jpg', `*Comando en proceso, puedes enviar tu imagen a .moderadora*`, m);
 };
 // conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['messi'];
